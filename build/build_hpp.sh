710198756c92a6c8a646242ac613728ddd60e37d
Llewellyn Falco <isidore@setgame.com>
2018-02-16 00:19:29 +0200

    v.0.3.0

--- build/build_hpp.sh
+++ build/build_hpp.sh
@@ -1,4 +1,4 @@
   cd ../ApprovalTests
-  java -jar ../build/SingleHpp.jar ../build/releases/ApprovalTests.v.0.2.0.hpp
+  java -jar ../build/SingleHpp.jar ../build/releases/ApprovalTests.v.0.3.0.hpp
 
-  cp ../build/releases/ApprovalTests.v.0.2.0.hpp ../../ApprovalTests.Cpp.StarterProject/lib
+  cp ../build/releases/ApprovalTests.v.0.3.0.hpp ../../ApprovalTests.Cpp.StarterProject/lib
