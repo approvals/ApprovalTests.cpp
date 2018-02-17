742702a4e21ed05a23f5ce116c14d76d88fa2b9a
Llewellyn Falco <isidore@setgame.com>
2018-02-17 19:30:54 +0200

    trying cpp 17 in tests

--- ApprovalTests_Catch2_Tests/VectorTests.cpp
+++ ApprovalTests_Catch2_Tests/VectorTests.cpp
@@ -4,14 +4,12 @@ using namespace std;
 
 TEST_CASE("YouCanVerifyIteratorRange") {
     std::vector<std::string> v{"hello", "world"};
-    Approvals::verifyAll<std::vector<std::string>::const_iterator>("FIRST LETTER",v.begin(), v.end(), [](std::string s, std::ostream& os){os << s << " => " << s[0];});
-    //Approvals::verifyAll("FIRST LETTER",v.begin(), v.end(), [](auto s, auto& os){os << s << " => " << s[0];});
+    Approvals::verifyAll("FIRST LETTER",v.begin(), v.end(), [](auto s, auto& os){os << s << " => " << s[0];});
 }
 
 TEST_CASE("YouCanVerifyVectors") {
     std::vector<std::string> v{"hello", "world"};
-    Approvals::verifyAll<std::vector<std::string>>("FIRST LETTER", v, [](std::string s, std::ostream& os){os << s << " => " << s[0];});
-    //  Approvals::verifyAll("FIRST LETTER", v, [](auto s, auto& os){os << s << " => " << s[0];});
+    Approvals::verifyAll("FIRST LETTER", v, [](auto s, auto& os){os << s << " => " << s[0];});
 }
 
 TEST_CASE("YouCanVerifyVectorsWithStandardText") {
