59e26ff4f9c7c14d8922fde64e76d9bfe977fca1
Llewellyn Falco <isidore@setgame.com>
2018-03-01 17:14:53 +0200

    version v.1.1.0

--- build/build_hpp.sh
+++ build/build_hpp.sh
@@ -1,4 +1,6 @@
-  cd ../ApprovalTests
-  java -jar ../build/SingleHpp.jar ../build/releases/ApprovalTests.v.1.1.0.hpp
+#!/bin/sh
+VERSION="v.1.1.0"
+cd ../ApprovalTests
+java -jar ../build/SingleHpp.v.0.0.2.jar ../build/releases/ApprovalTests.$VERSION.hpp
 
-  cp ../build/releases/ApprovalTests.v.1.1.0.hpp ../../ApprovalTests.Cpp.StarterProject/lib
+cp ../build/releases/ApprovalTests.$VERSION.hpp ../../ApprovalTests.Cpp.StarterProject/lib
