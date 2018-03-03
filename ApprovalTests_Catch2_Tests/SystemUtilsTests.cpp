71c18a5622e16ee7ec8ef84a8e6d78ea2848a82a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2018-03-03 13:55:02 +0000

    Save work-in-progress fixing windows filenames

---
+++ ApprovalTests_Catch2_Tests/SystemUtilsTests.cpp
@@ -0,0 +1,21 @@
+#include "Catch.hpp"
+#include "../ApprovalTests/SystemUtils.h"
+#include "../ApprovalTests/StringUtils.h"
+
+TEST_CASE("ItCanFixCaseOfFileNameOnWindows")
+{
+    if( SystemUtils::isWindowsOs())
+    {
+        const std::string original_filename = __FILE__;
+        const std::string lowercased_filename = StringUtils::toLower(original_filename);
+        const std::string fixed_filename = SystemUtils::checkFilenameCase(lowercased_filename);
+        INFO("The 'fixed' filename is " << fixed_filename);
+
+        REQUIRE(original_filename.length() == fixed_filename.length());
+        CHECK(StringUtils::contains(fixed_filename, "SystemUtilsTests.cpp"));
+        // The current implementation doesn't fix the case of folder names, only the file
+        // So for now, check that the folder name is still present, but expect it
+        // to be lower case
+        CHECK(StringUtils::contains(fixed_filename, "approvaltests_catch2_tests"));
+    }
+}
