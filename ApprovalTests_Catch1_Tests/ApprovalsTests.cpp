8d13749afa5de06418a0d343f5ad1858a5d65061
Llewellyn Falco <isidore@setgame.com>
2018-03-30 20:06:58 +0300

    Confirming it also works with Catch1
    Co-Authored-By: Phil Nash <github@philnash.me>

---
+++ ApprovalTests_Catch1_Tests/ApprovalsTests.cpp
@@ -0,0 +1,9 @@
+#include "../lib/catch.1.9.0.hpp"
+#include "../ApprovalTests/Approvals.h"
+
+using namespace std;
+
+TEST_CASE("YouCanVerifyText") {
+    Approvals::verify("My objects!");
+}
+
---
+++ ApprovalTests_Catch1_Tests/main.cpp
@@ -0,0 +1,6 @@
+#define CATCH_CONFIG_MAIN
+#include "../lib/catch.1.9.0.hpp"
+
+#define APPROVALS_CATCH
+#include "../ApprovalTests/Catch2Approvals.h"
+
