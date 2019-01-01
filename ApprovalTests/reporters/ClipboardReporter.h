1427e44fc7a88078f5112dd5803e97dee860a495
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-01-01 17:30:13 +0000

    Note that ClipboardReporter may not work on Linux

--- ApprovalTests/reporters/ClipboardReporter.h
+++ ApprovalTests/reporters/ClipboardReporter.h
@@ -26,6 +26,22 @@ public:
     }
 
     void copyToClipboard(const std::string& newClipboard) const {
+        /*
+         This will probably not work on Linux.
+
+         From https://stackoverflow.com/a/750466/104370
+         In case of X, yes, there's xclip (and others). xclip -selection c will send data to the clipboard that
+         works with Ctrl-C, Ctrl-V in most applications.
+
+         If you're trying to talk to the Mac OS X clipboard, there's pbcopy.
+
+         If you're in Linux terminal mode (no X) then maybe you need to look into gpm.
+
+         There's also GNU screen which has a clipboard. To put stuff in there, look at the screen command "readreg".
+
+         Under Windows/cygwin, use /dev/clipboard or clip for newer versions of Windows (at least Windows 10).
+         */
+
         const std::string clipboardCommand = SystemUtils::isWindowsOs() ? "clip" : "pbclip";
         auto cmd = std::string("echo ") + newClipboard + " | " + clipboardCommand;
 		system(cmd.c_str());
