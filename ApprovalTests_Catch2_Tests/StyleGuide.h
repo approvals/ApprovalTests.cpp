4351beb83a0e71b00e89ded9dc09853518351342
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-01-25 20:49:46 +0000

    d Added style guide
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

---
+++ ApprovalTests_Catch2_Tests/StyleGuide.h
@@ -0,0 +1,18 @@
+
+#ifndef APPROVALTESTS_CPP_STYLEGUIDE_H
+#define APPROVALTESTS_CPP_STYLEGUIDE_H
+
+// This example shows the naming conventions used in this project.
+namespace ExampleNameSpace {
+    class StyleGuide {
+        void methodName(int parameterOne) {
+            int localVariable = 0;
+        }
+
+    private:
+        int dataMember = 0;
+    };
+}
+
+
+#endif //APPROVALTESTS_CPP_STYLEGUIDE_H
