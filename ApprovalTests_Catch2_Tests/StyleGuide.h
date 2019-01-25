fc51b17549f6e4b0d33f80fb1500d37f1ba26944
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-01-25 21:10:28 +0000

    Update StyleGuide.h
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- ApprovalTests_Catch2_Tests/StyleGuide.h
+++ ApprovalTests_Catch2_Tests/StyleGuide.h
@@ -2,10 +2,15 @@
 #ifndef APPROVALTESTS_CPP_STYLEGUIDE_H
 #define APPROVALTESTS_CPP_STYLEGUIDE_H
 
+#define MACRO_NAME 1
+
 // This example shows the naming conventions used in this project.
-namespace ExampleNameSpace {
-    class StyleGuide {
-        void methodName(int parameterOne) {
+namespace ExampleNameSpace
+{
+    class StyleGuide
+    {
+        void methodName(int parameterOne)
+        {
             int localVariable = 0;
         }
 
