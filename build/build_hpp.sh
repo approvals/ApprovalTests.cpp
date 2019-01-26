add9c4e6a813f785069c586a4d8503c434a5f1b0
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-01-26 11:23:03 +0000

    e Record how to release on cygwin

--- build/build_hpp.sh
+++ build/build_hpp.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# to run this in cygwin (after installing dos2unix):
+#   cat build_hpp.sh | dos2unix | bash
+
 VERSION="v.2.1.0"
 cd ../ApprovalTests
 java -jar ../build/SingleHpp.v.0.0.2.jar ../build/releases/ApprovalTests.$VERSION.hpp
