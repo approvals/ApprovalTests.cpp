3892903c688af3e98d8dc8e2002ffc97c738e1bc
Clare Macrae <claremacrae@users.noreply.github.com>
2019-03-03 23:54:07 +0000

    d Note the need to re-install Markdown snippets tool

--- run_markdown_templates.sh
+++ run_markdown_templates.sh
@@ -1,4 +1,12 @@
 # install dotnet SDK from http://go.microsoft.com/fwlink/?LinkID=798306&clcid=0x409
 # Then install GitHubMarkdownSnippets with
 #   dotnet tool install -g GitHubMarkdownSnippets
+
+# TODO Update for the renaming of the project
+# See https://github.com/SimonCropp/MarkdownSnippets/issues/5
+# Use the following to change over:
+
+# dotnet tool uninstall -g GitHubMarkdownSnippets
+# dotnet tool install -g MarkdownSnippets.Tool
+
 mdsnippets
