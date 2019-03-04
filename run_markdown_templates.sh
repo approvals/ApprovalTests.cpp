586fcd13008f84121cb7074c7351b0225682c501
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-03-04 15:04:07 +0000

    d Remove reminder to update mdsnippets - now done

--- run_markdown_templates.sh
+++ run_markdown_templates.sh
@@ -2,16 +2,9 @@
 # For info on mdsnippets, see https://github.com/SimonCropp/MarkdownSnippets
 
 # install dotnet SDK from http://go.microsoft.com/fwlink/?LinkID=798306&clcid=0x409
-# Then install GitHubMarkdownSnippets with
-#   dotnet tool install -g GitHubMarkdownSnippets
+# Then install MarkdownSnippets.Tool with
+#   dotnet tool install -g MarkdownSnippets.Tool
 # To update:
-#   dotnet tool update  -g GitHubMarkdownSnippets
-
-# TODO Update for the renaming of the project
-# See https://github.com/SimonCropp/MarkdownSnippets/issues/5
-# Use the following to change over:
-
-# dotnet tool uninstall -g GitHubMarkdownSnippets
-# dotnet tool install -g MarkdownSnippets.Tool
+#   dotnet tool update  -g MarkdownSnippets.Tool
 
 mdsnippets
