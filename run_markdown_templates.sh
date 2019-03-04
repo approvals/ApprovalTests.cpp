0612fb81310737c9bd3b32876782a459a15dd814
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-03-04 14:59:02 +0000

    d Add some more notes on GitHubMarkdownSnippets

--- run_markdown_templates.sh
+++ run_markdown_templates.sh
@@ -1,6 +1,11 @@
+#!/usr/bin/env bash
+# For info on mdsnippets, see https://github.com/SimonCropp/MarkdownSnippets
+
 # install dotnet SDK from http://go.microsoft.com/fwlink/?LinkID=798306&clcid=0x409
 # Then install GitHubMarkdownSnippets with
 #   dotnet tool install -g GitHubMarkdownSnippets
+# To update:
+#   dotnet tool update  -g GitHubMarkdownSnippets
 
 # TODO Update for the renaming of the project
 # See https://github.com/SimonCropp/MarkdownSnippets/issues/5
