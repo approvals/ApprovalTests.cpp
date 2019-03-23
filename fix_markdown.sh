a29ba79bc7565c3889537bbb740f8c926fa9694e
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-03-23 11:03:25 +0000

    e Add check for headings not in table of contents

--- fix_markdown.sh
+++ fix_markdown.sh
@@ -2,6 +2,10 @@
 
 find . -name \*.md | grep -v googletest-src | xargs sed -i -f fix_markdown.sed
 
+echo
+echo "The following files, if any, have headings (Level 2 or below) not in the Contents list"
+find . -name \*.md | grep -v googletest-src | xargs grep -n '^##' | grep -v '<a name='
 
-echo "The following files, if any, are missing the 'Back to User Guide' line at the end:"
+echo
+echo "The following files in doc, if any, are missing the 'Back to User Guide' line at the end:"
 find ./doc -name \*.md | grep -v googletest-src | grep -v README.md | xargs grep -L 'Back to User Guide'
