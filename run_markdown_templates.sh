02f2096466c62837487e761c7ebcce4d83f7cfad
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-04-12 22:11:49 +0100

    e Automate updating of tables of contents
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- run_markdown_templates.sh
+++ run_markdown_templates.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
+
+# to run this in cygwin (after installing dos2unix):
+#   cat run_markdown_templates.sh | dos2unix | bash
+
+# ---------------------------------------------------
+# Update tables of contents
+# ---------------------------------------------------
+
+# To install:
+#   npm install -g doctoc
+
+doctoc --title **Contents** .
+
+# ---------------------------------------------------
+# Update code examples
+# ---------------------------------------------------
 # For info on mdsnippets, see https://github.com/SimonCropp/MarkdownSnippets
 
 # install dotnet SDK from http://go.microsoft.com/fwlink/?LinkID=798306&clcid=0x409
