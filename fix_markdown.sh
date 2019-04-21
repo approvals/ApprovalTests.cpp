666193a1aa663a13ca18200d5f6c5ff91128e67b
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-04-21 09:42:40 +0100

    e Remove a check of headings that's not needed since move to doctoc

--- fix_markdown.sh
+++ fix_markdown.sh
@@ -3,10 +3,6 @@
 find . -name \*.md | grep -v googletest-src | xargs sed -i -f fix_markdown.sed
 
 echo
-echo "The following files, if any, have headings (Level 2 or below) not in the Contents list"
-find . -name \*.md | grep -v googletest-src | xargs grep -n '^##' | grep -v '<a name=' | grep -v HowToRelease.md
-
-echo
 echo "The following files, if any, are missing their 'top' anchor:"
 find -name \*.md | grep -v googletest-src | grep -v README.md | xargs grep -L '<a id="top"></a>' | grep -v 'relnotes_'
 
