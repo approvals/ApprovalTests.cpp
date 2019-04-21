57ef69b69678a5af86f4f123e4da1990629bd50e
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-04-21 10:09:46 +0100

    e Update for the addition of README.source.md for User Guide

--- fix_markdown.sh
+++ fix_markdown.sh
@@ -4,11 +4,11 @@ find . -name \*.md | grep -v googletest-src | xargs sed -i -f fix_markdown.sed
 
 echo
 echo "The following files, if any, are missing their 'top' anchor:"
-find -name \*.md | grep -v googletest-src | grep -v README.md | xargs grep -L '<a id="top"></a>' | grep -v 'relnotes_'
+find -name \*.md | grep -v googletest-src | grep -v README.md | grep -v README.source.md | xargs grep -L '<a id="top"></a>' | grep -v 'relnotes_'
 
 echo
 echo "The following files in doc, if any, are missing the 'Back to User Guide' line at the end:"
-find ./doc -name \*.md | grep -v googletest-src | grep -v README.md | xargs grep -L 'Back to User Guide'
+find ./doc -name \*.md | grep -v googletest-src | grep -v README.md | grep -v README.source.md | xargs grep -L 'Back to User Guide'
 
 echo
 echo "The following files, if any, need to have relative references converted to absolute ones"
