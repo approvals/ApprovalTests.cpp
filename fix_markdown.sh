0254c665e82e319e3d4ceed70922175f7cfbfbfa
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-04-21 10:05:22 +0100

    e Don't check links in TestRelativeLinks files; treat mailto like http

--- fix_markdown.sh
+++ fix_markdown.sh
@@ -12,4 +12,4 @@ find ./doc -name \*.md | grep -v googletest-src | grep -v README.md | xargs grep
 
 echo
 echo "The following files, if any, need to have relative references converted to absolute ones"
-find -name \*.md | grep -v googletest-src | xargs grep -P '\]\((?!#|http|/)'
+find -name \*.md | grep -v googletest-src | grep -v TestRelativeLinks | xargs grep -P '\]\((?!#|http|/|mailto)'
