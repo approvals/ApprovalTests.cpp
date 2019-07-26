d7370ee96baa71ad80aac2eac5aa213fdffbdd64
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-07-26 19:19:33 +0100

    t Add first passing test for doctest namer
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- ApprovalTests_DocTest_Tests/DocTestNamerTests.cpp
+++ ApprovalTests_DocTest_Tests/DocTestNamerTests.cpp
@@ -4,3 +4,91 @@ TEST_CASE("first passing doctest test")
 {
     CHECK(1 == 1);
 }
+
+#include "ApprovalTests/namers/ApprovalTestNamer.h"
+//#include "ApprovalTests/StringUtils.h"
+//#include "ApprovalTests/Approvals.h"
+//
+//using namespace std;
+//using Catch::Matchers::EndsWith;
+//
+//TEST_CASE("ItCanGiveYouTheSpecName") {
+//
+//    ApprovalTestNamer namer;
+//    REQUIRE(namer.getTestName() == "ItCanGiveYouTheSpecName");
+//
+//    SECTION("andSectionNames") {
+//        REQUIRE(namer.getTestName() == "ItCanGiveYouTheSpecName.andSectionNames");
+//        SECTION("andEvenMoreSectionNames") {
+//            REQUIRE(namer.getTestName() == "ItCanGiveYouTheSpecName.andSectionNames.andEvenMoreSectionNames");
+//        }
+//    }
+//}
+//
+//
+TEST_CASE("ItCanGiveYouTheTestFileName") {
+    ApprovalTestNamer namer;
+    REQUIRE(namer.getFileName() == "DocTestNamerTests");
+}
+//
+//
+//TEST_CASE("TestProperNameCaseOnWindows") {
+//    if (SystemUtils::isWindowsOs())
+//    {
+//        ApprovalTestNamer namer;
+//        auto test = namer.currentTest();
+//        test.setFileName(StringUtils::toLower(test.getFileName()));
+//        namer.currentTest(&test);
+//        REQUIRE(namer.getFileName() == "NamerTests");
+//    }
+//}
+//
+//
+//TEST_CASE("ItCanGiveYouTheTestDirectory") {
+//    // This should work with CaseSensitive::Yes.
+//    // However, it would fail when run in Visual Studio 2017 as lower-case source-file names are returned.
+//    // We've fixed this for filenames, but not directory names, so this test ignores case.
+//    // See https://stackoverflow.com/questions/49068785/how-to-find-the-filename-from-a-c11-stat-objects-file-serial-number-on-window
+//    auto suppress_subdirectory = Approvals::useApprovalsSubdirectory("");
+//    ApprovalTestNamer namer;
+//    auto __ = SystemUtils::getDirectorySeparator();
+//    REQUIRE_THAT(namer.getDirectory(), EndsWith(__ + "ApprovalTests_Catch2_Tests" + __, Catch::CaseSensitive::No));
+//}
+//
+//
+//TEST_CASE("ItIncludesFileContextAndSpecNames") {
+//    ApprovalTestNamer namer;
+//    auto __ = SystemUtils::getDirectorySeparator();
+//
+//    REQUIRE_THAT(namer.getApprovedFile(".txt"),
+//                 EndsWith(__ + "NamerTests.ItIncludesFileContextAndSpecNames.approved.txt"));
+//    REQUIRE_THAT(namer.getReceivedFile(".txt"),
+//                 EndsWith(__ + "NamerTests.ItIncludesFileContextAndSpecNames.received.txt"));
+//}
+//
+//
+//TEST_CASE("Clean Up Filename Transforms")
+//{
+//    std::vector<std::string> names = { "CleanUpFilenameTransforms", "Spaces In File \\" };
+//    Approvals::verifyAll("File Names", names, [&](std::string name, std::ostream &s) {s << name << " => " << ApprovalTestNamer::convertToFileName(name); });
+//}
+//
+//TEST_CASE("Use sub-directory")
+//{
+//    auto subdirectory = Approvals::useApprovalsSubdirectory("approved_files");
+//    auto namer = Approvals::getDefaultNamer();
+//    REQUIRE_THAT( namer->getApprovedFile(".txt"), Catch::Matchers::Contains( "approved_files" ) );
+//}
+//
+//TEST_CASE("Use sub-directories clean to previous results")
+//{
+//    auto subdirectory = Approvals::useApprovalsSubdirectory("outer");
+//    auto namer = Approvals::getDefaultNamer();
+//
+//    {
+//        auto subdirectory2 = Approvals::useApprovalsSubdirectory("inner");
+//        REQUIRE_THAT( namer->getApprovedFile(".txt"), Catch::Matchers::Contains( "inner" ) );
+//    }
+//
+//    REQUIRE_THAT( namer->getApprovedFile(".txt"), Catch::Matchers::Contains( "outer" ) );
+//}
--- ApprovalTests_DocTest_Tests/main.cpp
+++ ApprovalTests_DocTest_Tests/main.cpp
@@ -1,2 +1,148 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest.hpp"
+
+#include "../ApprovalTests/namers/ApprovalTestNamer.h"
+
+using namespace doctest;
+
+struct DocTestApprovalListener : IReporter
+{
+//    using TestEventListenerBase::TestEventListenerBase;
+    TestName currentTest;
+
+    std::ostream&         stdout_stream;
+    const ContextOptions& opt;
+    const TestCaseData*   tc;
+    std::mutex            mutex;
+
+    // constructor has to accept the ContextOptions by ref as a single argument
+    DocTestApprovalListener(const ContextOptions& in)
+        : stdout_stream(*in.cout)
+        , opt(in)
+        , tc(nullptr)
+    {
+        stdout_stream << "DocTestApprovalListener created ***************************************** \n";
+    }
+    
+    void test_case_start(const TestCaseData& testInfo) override
+    {
+        currentTest.setFileName(testInfo.m_file);
+        ApprovalTestNamer::currentTest(&currentTest);
+        stdout_stream << "file name " << currentTest.getFileName() << '\n';
+    }
+
+    void test_case_end(const CurrentTestCaseStats& /*in*/) override
+    {
+        while (!currentTest.sections.empty()) {
+            currentTest.sections.pop_back();
+        }
+    }
+
+    void report_query(const QueryData &data) override
+    {
+
+    }
+
+    void test_run_start() override
+    {
+
+    }
+
+    void test_run_end(const TestRunStats &stats) override
+    {
+
+    }
+
+    void test_case_exception(const TestCaseException &exception) override
+    {
+
+    }
+
+    void subcase_start(const SubcaseSignature &signature) override
+    {
+        currentTest.sections.push_back(signature.m_name);
+
+    }
+
+    void subcase_end() override
+    {
+        currentTest.sections.pop_back();
+    }
+
+    void log_assert(const AssertData &data) override
+    {
+
+    }
+
+    void log_message(const MessageData &data) override
+    {
+
+    }
+
+    void test_case_skipped(const TestCaseData &data) override
+    {
+
+    }
+
+//    void subcase_start(const SubcaseSignature& /*in*/) override {
+//        std::lock_guard<std::mutex> lock(mutex);
+//    }
+//
+//    void subcase_end() override {
+//        std::lock_guard<std::mutex> lock(mutex);
+//    }
+    
+//    Catch2ApprovalListener(DocTest::ReporterConfig const &_config) : DocTest::TestEventListenerBase(_config) {}
+//
+//    virtual void testCaseStarting(DocTest::TestCaseInfo const &testInfo) override {
+//
+//        currentTest.setFileName(testInfo.lineInfo.file);
+//        ApprovalTestNamer::currentTest(&currentTest);
+//    }
+//
+//    virtual void testCaseEnded(DocTest::TestCaseStats const &/*testCaseStats*/) override {
+//        while (!currentTest.sections.empty()) {
+//            currentTest.sections.pop_back();
+//        }
+//    }
+//
+//    virtual void sectionStarting(DocTest::SectionInfo const &sectionInfo) override {
+//        currentTest.sections.push_back(sectionInfo.name);
+//    }
+//
+//    virtual void sectionEnded(DocTest::SectionStats const &/*sectionStats*/) override {
+//        currentTest.sections.pop_back();
+//    }
+};
+//CATCH_REGISTER_LISTENER(Catch2ApprovalListener)
+
+REGISTER_REPORTER("approvals", 0, DocTestApprovalListener);
+
+//int main(int argc, char** argv) {
+//    doctest::Context context;
+//
+//    // !!! THIS IS JUST AN EXAMPLE SHOWING HOW DEFAULTS/OVERRIDES ARE SET !!!
+//
+//    // defaults
+//    context.applyCommandLine(argc, argv);
+//
+//    // overrides
+//
+//    int res = context.run(); // run queries, or run tests unless --no-run is specified
+//
+//    if(context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
+//        return res;          // propagate the result of the tests
+//
+//    context.clearFilters(); // removes all filters added up to this point
+//
+//    // your program - if the testing framework is integrated in your production code
+//
+//    return res; // the result from doctest is propagated here as well
+//}
+int main(int argc, char** argv)
+{
+    char *myargv[] = {"program name", "--reporters=approvals", NULL};
+    int myargc = sizeof(myargv) / sizeof(char*) - 1;
+
+    return doctest::Context(myargc, myargv).run();
+}
