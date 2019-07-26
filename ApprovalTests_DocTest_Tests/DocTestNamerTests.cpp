781b5566a1f1892dc4ea633152e4d19056e93342
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-07-26 20:38:41 +0100

    t Tidy up doctest tests
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- ApprovalTests_DocTest_Tests/DocTestNamerTests.cpp
+++ ApprovalTests_DocTest_Tests/DocTestNamerTests.cpp
@@ -1,65 +1,29 @@
 #include "doctest.hpp"
-
-TEST_CASE("first passing doctest test")
-{
-    CHECK(1 == 1);
-}
-
 #include "ApprovalTests/namers/ApprovalTestNamer.h"
-//#include "ApprovalTests/StringUtils.h"
 #include "ApprovalTests/Approvals.h"
-//
-//using namespace std;
-//using Catch::Matchers::EndsWith;
-//
-TEST_CASE("ItCanGiveYouTheSpecName") {
 
+TEST_CASE("ItCanGiveYouTheSpecName")
+{
     ApprovalTestNamer namer;
     REQUIRE(namer.getTestName() == "ItCanGiveYouTheSpecName");
 
-    DOCTEST_SUBCASE("andSectionNames") {
+    DOCTEST_SUBCASE("andSectionNames")
+    {
         REQUIRE(namer.getTestName() == "ItCanGiveYouTheSpecName.andSectionNames");
-        DOCTEST_SUBCASE("andEvenMoreSectionNames") {
+        DOCTEST_SUBCASE("andEvenMoreSectionNames")
+        {
             REQUIRE(namer.getTestName() == "ItCanGiveYouTheSpecName.andSectionNames.andEvenMoreSectionNames");
         }
     }
 }
 
-TEST_CASE("ItCanGiveYouTheTestFileName") {
+TEST_CASE("ItCanGiveYouTheTestFileName")
+{
     ApprovalTestNamer namer;
     REQUIRE(namer.getFileName() == "DocTestNamerTests");
 }
 
 TEST_CASE("It can verify tests with spaces")
 {
-    // DON'T SAVE REPORTER!!!!!
     Approvals::verify("hello world");
-//    Approvals::verify("hello world", Windows::AraxisMergeReporter());
 }
-
-
-//TEST_CASE("Clean Up Filename Transforms")
-//{
-//    std::vector<std::string> names = { "CleanUpFilenameTransforms", "Spaces In File \\" };
-//    Approvals::verifyAll("File Names", names, [&](std::string name, std::ostream &s) {s << name << " => " << ApprovalTestNamer::convertToFileName(name); });
-//}
-//
-//TEST_CASE("Use sub-directory")
-//{
-//    auto subdirectory = Approvals::useApprovalsSubdirectory("approved_files");
-//    auto namer = Approvals::getDefaultNamer();
-//    REQUIRE_THAT( namer->getApprovedFile(".txt"), Catch::Matchers::Contains( "approved_files" ) );
-//}
-//
-//TEST_CASE("Use sub-directories clean to previous results")
-//{
-//    auto subdirectory = Approvals::useApprovalsSubdirectory("outer");
-//    auto namer = Approvals::getDefaultNamer();
-//
-//    {
-//        auto subdirectory2 = Approvals::useApprovalsSubdirectory("inner");
-//        REQUIRE_THAT( namer->getApprovedFile(".txt"), Catch::Matchers::Contains( "inner" ) );
-//    }
-//
-//    REQUIRE_THAT( namer->getApprovedFile(".txt"), Catch::Matchers::Contains( "outer" ) );
-//}
