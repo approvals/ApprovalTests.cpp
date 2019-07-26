832a1cf9aca0b3e34e2dfdd68c2b6e0208a51154
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-07-26 20:20:59 +0100

    t Delete unused stub class
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- ApprovalTests_DocTest_Tests/main.cpp
+++ ApprovalTests_DocTest_Tests/main.cpp
@@ -5,58 +5,6 @@
 
 using namespace doctest;
 
-struct DocTestReporterStub : IReporter
-{
-    DocTestReporterStub()
-    {
-    }
-
-    void test_case_start(const TestCaseData& /*testInfo*/) override
-    {
-    }
-
-    void test_case_end(const CurrentTestCaseStats& /*in*/) override
-    {
-    }
-
-    void report_query(const QueryData &/*data*/) override
-    {
-    }
-
-    void test_run_start() override
-    {
-    }
-
-    void test_run_end(const TestRunStats &/*stats*/) override
-    {
-    }
-
-    void test_case_exception(const TestCaseException &/*exception*/) override
-    {
-    }
-
-    void subcase_start(const SubcaseSignature &/*signature*/) override
-    {
-
-    }
-
-    void subcase_end() override
-    {
-    }
-
-    void log_assert(const AssertData &/*data*/) override
-    {
-    }
-
-    void log_message(const MessageData &/*data*/) override
-    {
-    }
-
-    void test_case_skipped(const TestCaseData &/*data*/) override
-    {
-    }
-};
-
 struct DocTestApprovalListener : ConsoleReporter
 {
     TestName currentTest;
