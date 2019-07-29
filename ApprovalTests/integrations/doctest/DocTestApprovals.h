3d0d9ecf7241fa08357a91af72c7ab6d7df67850
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-07-29 18:02:26 +0100

    B Fix prevent compiler -Wsubobject-linkage warnings
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- ApprovalTests/integrations/doctest/DocTestApprovals.h
+++ ApprovalTests/integrations/doctest/DocTestApprovals.h
@@ -9,47 +9,52 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include <doctest.hpp>
 
-struct DocTestApprovalListener : doctest::ConsoleReporter
+// anonymous namespace to prevent compiler -Wsubobject-linkage warnings
+// This is OK as this code is only compiled on main()
+namespace
 {
-    TestName currentTest;
-
-    // constructor has to accept the ContextOptions by ref as a single argument
-    DocTestApprovalListener(const doctest::ContextOptions& in) : ConsoleReporter(in)
+    struct DocTestApprovalListener : doctest::ConsoleReporter
     {
-    }
-
-    void test_case_start(const doctest::TestCaseData& testInfo) override
-    {
-        ConsoleReporter::test_case_start(testInfo);
-
-        currentTest.sections.push_back(testInfo.m_name);
-        currentTest.setFileName(testInfo.m_file);
-        ApprovalTestNamer::currentTest(&currentTest);
-    }
-
-    void test_case_end(const doctest::CurrentTestCaseStats& in) override
-    {
-        ConsoleReporter::test_case_end(in);
-
-        while (!currentTest.sections.empty()) {
+        TestName currentTest;
+    
+        // constructor has to accept the ContextOptions by ref as a single argument
+        DocTestApprovalListener(const doctest::ContextOptions& in) : ConsoleReporter(in)
+        {
+        }
+    
+        void test_case_start(const doctest::TestCaseData& testInfo) override
+        {
+            ConsoleReporter::test_case_start(testInfo);
+    
+            currentTest.sections.push_back(testInfo.m_name);
+            currentTest.setFileName(testInfo.m_file);
+            ApprovalTestNamer::currentTest(&currentTest);
+        }
+    
+        void test_case_end(const doctest::CurrentTestCaseStats& in) override
+        {
+            ConsoleReporter::test_case_end(in);
+    
+            while (!currentTest.sections.empty()) {
+                currentTest.sections.pop_back();
+            }
+        }
+    
+        void subcase_start(const doctest::SubcaseSignature &signature) override
+        {
+            ConsoleReporter::subcase_start(signature);
+    
+            currentTest.sections.push_back(signature.m_name);
+        }
+    
+        void subcase_end() override
+        {
+            ConsoleReporter::subcase_end();
+    
             currentTest.sections.pop_back();
         }
-    }
-
-    void subcase_start(const doctest::SubcaseSignature &signature) override
-    {
-        ConsoleReporter::subcase_start(signature);
-
-        currentTest.sections.push_back(signature.m_name);
-    }
-
-    void subcase_end() override
-    {
-        ConsoleReporter::subcase_end();
-
-        currentTest.sections.pop_back();
-    }
-};
+    };
+}
 
 REGISTER_REPORTER("approvals", 0, DocTestApprovalListener);
 
