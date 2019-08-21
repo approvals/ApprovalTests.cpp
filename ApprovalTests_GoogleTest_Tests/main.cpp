ba203a8002eddfdc6a2b0ddc95dacbcc6c0d2707
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-08-21 21:45:56 +0100

    t Put approved files in sub-directory

--- ApprovalTests_GoogleTest_Tests/main.cpp
+++ ApprovalTests_GoogleTest_Tests/main.cpp
@@ -4,3 +4,5 @@
 #define APPROVALS_GOOGLETEST // This tells Approval Tests to provide a main() - only do this in one cpp file
 #include "ApprovalTests.hpp"
 // end-snippet
+
+auto directory = Approvals::useApprovalsSubdirectory("approval_tests");
