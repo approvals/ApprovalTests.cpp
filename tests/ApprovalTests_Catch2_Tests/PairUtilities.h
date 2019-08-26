441fd1d7240490c3766bfd8c5c38174bf12bbb4d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-08-26 13:48:15 +0100

    r Try fixing Visual Studio build errors in CombinationTests.cpp

--- tests/ApprovalTests_Catch2_Tests/PairUtilities.h
+++ tests/ApprovalTests_Catch2_Tests/PairUtilities.h
@@ -1,9 +1,9 @@
 #include <map>
 #include <ostream>
 
-inline std::ostream &operator<<(std::ostream &os, const std::pair<std::string, int>& pair)
-{
+namespace ApprovalTests {
+inline std::ostream &operator<<(std::ostream &os, const std::pair<std::string, int> &pair) {
     os << "(" << pair.first << ", " << pair.second << ")";
     return os;
 }
-
+}
