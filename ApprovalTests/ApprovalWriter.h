5f3628b1763068a156530641f989f72a66813732
Alastair J. Taylor <alastair@transfinite.com>
2019-08-28 14:35:14 +0100

    Polymorphic class with non-virtual public destructor Thanks to https://github.com/alastairUK/ApprovalTests.cpp/commit/4cc4c02c9b007ed1c15f7f87ee592a00a1602f93

--- ApprovalTests/ApprovalWriter.h
+++ ApprovalTests/ApprovalWriter.h
@@ -5,6 +5,7 @@ namespace ApprovalTests {
 class ApprovalWriter
 {
 public:
+    virtual ~ApprovalWriter() = default;
     virtual std::string getFileExtensionWithDot() const = 0;
     virtual void write(std::string path) const = 0;
     virtual void cleanUpReceived(std::string receivedPath) const = 0;
