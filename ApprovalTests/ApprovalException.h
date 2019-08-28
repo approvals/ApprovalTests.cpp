cdb1a0f4c5da3590303e29da46ac134722446cd1
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-08-28 14:42:21 +0100

    Dynamic exception specification 'throw()' is deprecated;  consider using 'noexcept' instead [hicpp-use-noexcept] Thanks to https://github.com/alastairUK/ApprovalTests.cpp/commit/620a75f6560763b35b39b1a575169e45c0a2433b

--- ApprovalTests/ApprovalException.h
+++ ApprovalTests/ApprovalException.h
@@ -13,7 +13,7 @@ private:
 public:
     ApprovalException( const std::string& msg ) : message( msg ) {}
 
-    virtual const char *what() const throw() override
+    virtual const char *what() const noexcept override
     {
         return message.c_str();
     }
