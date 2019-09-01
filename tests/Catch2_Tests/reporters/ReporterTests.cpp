6ae9adc5061bc9117fefeb5ee3aa70dd3f713bab
alastairUK <alastairUK@users.noreply.github.com>
2019-09-01 14:00:39 +0100

    Catch2: [modernize-raw-string-literal]

--- tests/Catch2_Tests/reporters/ReporterTests.cpp
+++ tests/Catch2_Tests/reporters/ReporterTests.cpp
@@ -35,7 +35,7 @@ TEST_CASE("Reporters Report Failure Status") {
 }
 
 TEST_CASE("Reporters Report Success Status") {
-    std::string knownCommand = ApprovalTests::SystemUtils::isWindowsOs() ? "C:\\Windows\\System32\\help.exe" : "echo";
+    std::string knownCommand = ApprovalTests::SystemUtils::isWindowsOs() ? R"(C:\Windows\System32\help.exe)" : "echo";
     ApprovalTests::GenericDiffReporter m(knownCommand);
     bool result = m.report("r.txt", "a.txt");
     REQUIRE(true == result);
