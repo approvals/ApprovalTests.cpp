05f1f343652768c008c82aba5c38eacd6cc043a6
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-09-03 07:42:42 +0100

    e Make it easy to link to sections of pages

--- doc/mdsource/link_page.sh
+++ doc/mdsource/link_page.sh
@@ -10,10 +10,24 @@
 
 # TODO Document this in Contributing page
 
+# Force execution to halt if there are any errors in this script:
+set -e
+set -o pipefail
+
+echo
 for source_file in "$@"
 do
     doc_base=$(echo "$source_file" | sed -e "s/.source.md//")
     doc_title=$(grep '^# ' "$source_file" | head -1 | sed -e "s/^# //")
     doc_abs_path="/doc/$doc_base.md"
+    doc_rel_path="../$doc_base.md"
+
+    # Write out the link to use to the top of the file
     echo "[$doc_title]($doc_abs_path#top)"
+
+    # Write out links to all the entries in the table of contents
+    sed -e '/-- endtoc --/q' "$doc_rel_path" | grep '* \[' | sed -e "s/^ *\* //" | sed -e "s|#|$doc_abs_path#|"
+
+    # Add blank line, for readability if we are linking to multiple files
+    echo
 done
