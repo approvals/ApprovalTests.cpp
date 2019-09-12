c705d476db5b9cc7e359f1516e02bd176d4c5c44
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-09-12 13:30:18 -0700

    e Fix errors in fix_markdown.sh - sed change means it only runs on Mac

--- fix_markdown.sh
+++ fix_markdown.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-find . -name \*.md | grep -v googletest-src | xargs sed -i -f fix_markdown.sed
+find . -name \*.md | grep -v googletest-src | xargs sed -i '' -f fix_markdown.sed
 
 echo
 echo "The following files, if any, are missing their 'top' anchor:"
-find -name \*.md | grep -v googletest-src | grep -v README.md | grep -v README.source.md | xargs grep -L '<a id="top"></a>' | grep -v 'relnotes_'
+find . -name \*.md | grep -v googletest-src | grep -v README.md | grep -v README.source.md | xargs grep -L '<a id="top"></a>' | grep -v 'relnotes_'
 
 echo
 echo "The following files in doc, if any, are missing the 'Back to User Guide' line at the end:"
@@ -12,7 +12,7 @@ find ./doc -name \*.md | grep -v googletest-src | grep -v README.md | grep -v RE
 
 echo
 echo "The following files, if any, need to have relative references converted to absolute ones"
-find -name \*.source.md | grep -v googletest-src | xargs grep -P '\]\((?!#|http|/|mailto)' | grep -v '[this link is wrong]'
+find . -name \*.source.md | grep -v googletest-src | xargs grep -P '\]\((?!#|http|/|mailto)' | grep -v '[this link is wrong]'
 
 echo
 echo "The following files, if any, are not in the User Guide (doc/README.md)"
