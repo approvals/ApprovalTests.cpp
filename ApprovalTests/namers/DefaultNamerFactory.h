080849609038437daa93c1b94cef6ba4a60015c8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-09-21 21:07:23 -0600

    r Greatly simplify DefaultNamerFactory

--- ApprovalTests/namers/DefaultNamerFactory.h
+++ ApprovalTests/namers/DefaultNamerFactory.h
@@ -6,7 +6,6 @@
 
 #include <memory>
 #include <utility>
-#include <vector>
 
 namespace ApprovalTests {
 
@@ -16,29 +15,21 @@ namespace ApprovalTests {
 class DefaultNamerFactory
 {
 private:
-    using NamerContainer = std::vector< NamerCreator >;
-    static NamerContainer& defaultNamerContainer()
+    static NamerCreator& defaultNamer()
     {
-        static NamerContainer namers = *createNamerContainer();
-        return namers;
-    }
-
-    static NamerContainer* createNamerContainer()
-    {
-        auto container = new NamerContainer; 
-        container->push_back( [](){return std::make_shared<ApprovalTestNamer>();} );
-        return container;
+        static NamerCreator namer = [](){return std::make_shared<ApprovalTestNamer>();};
+        return namer;
     }
 
 public:
     static NamerCreator getDefaultNamer()
     {
-        return defaultNamerContainer().at(0);
+        return defaultNamer();
     }
     
     static void setDefaultNamer( NamerCreator namer)
     {
-        defaultNamerContainer().at(0) = std::move(namer);
+        defaultNamer() = std::move(namer);
     }
 
 };
