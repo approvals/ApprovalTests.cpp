dcb96eda9b58b3b46b956b50aec8949a38689ecf
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-10-04 03:54:17 -0600

    c Add explanatory comment

--- ApprovalTests/CombinationApprovals.h
+++ ApprovalTests/CombinationApprovals.h
@@ -154,7 +154,8 @@ increment_iterator(Its& its, const Its& begins, const Its& ends) {
 
 // This is what actually loops over all the containers, one element at a time
 // It is called with a template type F that writes the inputs, and runs the converter, which writes the result(s)
-// all for one set of container values.
+// all for one set of container values - when called by verifyAllCombinations()
+// More generally, F must have an operator() that acts on one set of input values.
 template<class F, class... Ranges>
 void cartesian_product(F&& f, const Ranges&... ranges) {
     using std::begin;
