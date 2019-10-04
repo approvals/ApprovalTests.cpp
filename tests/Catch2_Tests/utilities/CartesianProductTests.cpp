16bd856f40ec97cc28df921ba735bc3c789fa789
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-10-03 21:18:13 -0600

    t Improve function name

--- tests/Catch2_Tests/utilities/CartesianProductTests.cpp
+++ tests/Catch2_Tests/utilities/CartesianProductTests.cpp
@@ -21,7 +21,7 @@ struct accumulate_results
 };
 
 template<class Converter, class Container, class... Containers>
-void run_cartesian_product(const Result& expected, Converter&& converter, const Container& input0, const Containers&... inputs)
+void test_cartesian_product(const Result& expected, Converter&& converter, const Container& input0, const Containers&... inputs)
 {
     auto results_store = accumulate_results<Converter>{
         Result(),
@@ -34,7 +34,7 @@ TEST_CASE("Single Vector-Single Value")
 {
     std::vector<std::string> words{"hello"};
     Result expected{"hello!"};
-    run_cartesian_product(expected, [](const std::string& s){return s + "!";}, words);
+    test_cartesian_product(expected, [](const std::string& s){return s + "!";}, words);
 }
 
 TEST_CASE("Two Vectors-Single Value")
@@ -42,7 +42,7 @@ TEST_CASE("Two Vectors-Single Value")
     std::vector<std::string> input1{"hello"};
     std::vector<std::string> input2{"world"};
     Result expected{"hello,world"};
-    run_cartesian_product(expected, [](const std::string& s1, const std::string& s2){return s1 + "," + s2;}, input1, input2);
+    test_cartesian_product(expected, [](const std::string& s1, const std::string& s2){return s1 + "," + s2;}, input1, input2);
 }
 
 TEST_CASE("Cartesian product works with vector input")
@@ -50,7 +50,7 @@ TEST_CASE("Cartesian product works with vector input")
     std::vector<std::string> input1{"A", "B"};
     std::vector<std::string> input2{"1", "2"};
     Result expected{"A,1", "A,2", "B,1", "B,2"};
-    run_cartesian_product(expected, [](const std::string& s1, const std::string& s2){return s1 + "," + s2;}, input1, input2);
+    test_cartesian_product(expected, [](const std::string& s1, const std::string& s2){return s1 + "," + s2;}, input1, input2);
 }
 
 // TODO Rename this to describe the type of iterator
@@ -59,7 +59,7 @@ TEST_CASE("Cartesian product works with set input")
     std::set<std::string> input1{"A", "B"};
     std::set<std::string> input2{"1", "2"};
     Result expected{"A,1", "A,2", "B,1", "B,2"};
-    run_cartesian_product(expected, [](const std::string& s1, const std::string& s2){return s1 + "," + s2;}, input1, input2);
+    test_cartesian_product(expected, [](const std::string& s1, const std::string& s2){return s1 + "," + s2;}, input1, input2);
 }
 
 TEST_CASE("Cartesian product works with mixed inputs")
@@ -67,7 +67,7 @@ TEST_CASE("Cartesian product works with mixed inputs")
     std::vector<std::string> input1{"hello"};
     std::set<std::string> input2{"world"};
     Result expected{"hello,world"};
-    run_cartesian_product(expected, [](const std::string& s1, const std::string& s2){return s1 + "," + s2;}, input1, input2);
+    test_cartesian_product(expected, [](const std::string& s1, const std::string& s2){return s1 + "," + s2;}, input1, input2);
 }
 
 TEST_CASE("Cartesian product wwith an empty input gives empty output")
@@ -75,5 +75,5 @@ TEST_CASE("Cartesian product wwith an empty input gives empty output")
     std::set<std::string> input1{"A", "B"};
     std::set<std::string> input2;
     Result expected;
-    run_cartesian_product(expected, [](const std::string& s1, const std::string& s2){return s1 + "," + s2;}, input1, input2);
+    test_cartesian_product(expected, [](const std::string& s1, const std::string& s2){return s1 + "," + s2;}, input1, input2);
 }
