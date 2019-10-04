003f116cb7ab7ebf659be2ceed477ccc73c6229d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-10-03 20:06:23 -0600

    t Prepare to simplify local copy of serialize, from CombinationApprovals.h

--- tests/Catch2_Tests/utilities/CartesianProductTests.cpp
+++ tests/Catch2_Tests/utilities/CartesianProductTests.cpp
@@ -4,11 +4,26 @@
 using namespace ApprovalTests;
 using namespace ApprovalTests::CombinationApprovals;
 
+template<class Converter>
+struct serialize
+{
+    std::ostream& out;
+    Converter converter;
+    template<class T, class... Ts>
+    void operator()(T&& input1, Ts&&... inputs) {
+        // First value is printed without trailing comma
+        out << "(" << input1;
+        // Remaining values are printed with prefix of a comma
+        for_each(std::forward_as_tuple(inputs...), Detail::print_input{out});
+        out << ") => " << converter(input1, inputs...) << '\n';
+    }
+};
+
 template<class Converter, class Container, class... Containers>
 std::string run_cartesian_product(Converter&& converter, const Container& input0, const Containers&... inputs)
 {
     std::stringstream s;
-    Detail::cartesian_product(Detail::serialize<Converter>{s, std::forward<Converter>(converter)}, input0, inputs...);
+    Detail::cartesian_product(serialize<Converter>{s, std::forward<Converter>(converter)}, input0, inputs...);
     return s.str();
 }
 
