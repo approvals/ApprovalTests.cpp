87d24dedc181f23287c9bff824051ecfb0ccf917
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-12-06 09:48:42 +0000

    c Fix typo in a comment

--- ApprovalTests/utilities/Macros.h
+++ ApprovalTests/utilities/Macros.h
@@ -1,7 +1,7 @@
 #ifndef APPROVALTESTS_CPP_MACROS_H
 #define APPROVALTESTS_CPP_MACROS_H
 
-// Use this in places where we have parameters that sometimes unused,
+// Use this in places where we have parameters that are sometimes unused,
 // e.g. because of #if
 // See https://stackoverflow.com/a/1486931/104370
 #define APPROVAL_TESTS_UNUSED(expr) do { (void)(expr); } while (0)
