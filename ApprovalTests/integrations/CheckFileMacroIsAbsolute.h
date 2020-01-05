e7def7b43f977bcf5fef255dfe97f6d912c8f868
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-01-05 23:25:28 +0000

    F Try using a raw string literal for better compilation error message

--- ApprovalTests/integrations/CheckFileMacroIsAbsolute.h
+++ ApprovalTests/integrations/CheckFileMacroIsAbsolute.h
@@ -6,14 +6,22 @@ static_assert(__FILE__[1] == ':',
               "Please compile with /FC, see <link> for details");
 #else
 static_assert(__FILE__[0] == '/',
-              "\nApprovalTests requires an absolute path to source file "
-              "locations - not the relative one given by this build.\n"
-              "A common cause of this problem is using Ninja with in-tree "
-              "builds.\n"
-              "Please see "
-              "https://github.com/approvals/ApprovalTests.cpp/blob/master/doc/"
-              "TroubleshootingMisconfiguredBuild.md#top\n"
-              "for help.\n\n");
+              R"(
+********************************************************************************
+* Welcome to Approval Tests.
+*
+* There seems to be a problem with your build configuration:
+* we cannot find the test source file.
+*
+* ApprovalTests requires an absolute path to source file locations - not the
+* relative one given by this build.
+*
+* A common cause of this problem is using Ninja with in-tree builds.
+*
+* For details on how to fix this, please visit:
+* https://github.com/approvals/ApprovalTests.cpp/blob/master/doc/TroubleshootingMisconfiguredBuild.md
+********************************************************************************
+)");
 #endif
 
 #endif //APPROVALTESTS_CPP_CHECKFILEMACROISABSOLUTE_H
