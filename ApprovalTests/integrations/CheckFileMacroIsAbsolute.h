e524bd364da0b57a04ca172d10b4e471f578ce32
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-01-06 15:44:57 +0000

    r Shorten the Help message for Ninja problems to one line.

--- ApprovalTests/integrations/CheckFileMacroIsAbsolute.h
+++ ApprovalTests/integrations/CheckFileMacroIsAbsolute.h
@@ -1,26 +1,19 @@
 #ifndef APPROVALTESTS_CPP_CHECKFILEMACROISABSOLUTE_H
 #define APPROVALTESTS_CPP_CHECKFILEMACROISABSOLUTE_H
 
+// Maintenance note: the following help message must be short, and
+// not contain any newline characters, as their display looks messy
+// in some compiler outputs.
+
 #ifdef _MSC_VER
 static_assert(__FILE__[1] == ':',
 #else
 static_assert(__FILE__[0] == '/',
 #endif
-              R"(
-********************************************************************************
-* Welcome to Approval Tests.
-*
-* There seems to be a problem with your build configuration:
-* we cannot find the test source file.
-*
-* ApprovalTests requires an absolute path to source file locations - not the
-* relative one given by this build.
-*
-* A common cause of this problem is using Ninja with in-tree builds.
-*
-* For details on how to fix this, please visit:
-* https://github.com/approvals/ApprovalTests.cpp/blob/master/doc/TroubleshootingMisconfiguredBuild.md
-********************************************************************************
-)");
+// clang-format off
+"There seems to be a problem with your build configuration, probably with Ninja. "
+"Please visit https://github.com/approvals/ApprovalTests.cpp/blob/master/doc/TroubleshootingMisconfiguredBuild.md"
+// clang-format on
+    );
 
 #endif //APPROVALTESTS_CPP_CHECKFILEMACROISABSOLUTE_H
