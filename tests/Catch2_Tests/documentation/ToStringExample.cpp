7e5617d9031104d5c5e22db01fe48418b25cc6e6
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-01-14 15:57:12 +0000

    B Fix symbol conflict to fix Unity builds

--- tests/Catch2_Tests/documentation/ToStringExample.cpp
+++ tests/Catch2_Tests/documentation/ToStringExample.cpp
@@ -5,14 +5,14 @@
 
 using namespace ApprovalTests;
 
-struct Rectangle2
+struct Rectangle1
 {
 
     int x, y, width, height;
 
     // begin-snippet: to_string_standard_example
     friend std::ostream& operator<<(std::ostream& os,
-                                    const Rectangle2& rectangle)
+                                    const Rectangle1& rectangle)
     {
         os << "[x: " << rectangle.x << " y: " << rectangle.y
            << " width: " << rectangle.width << " height: " << rectangle.height
@@ -23,6 +23,6 @@ struct Rectangle2
 };
 TEST_CASE("ToStringsAreHelpful")
 {
-    Rectangle2 r = {40, 50, 100, 60};
+    Rectangle1 r = {40, 50, 100, 60};
     Approvals::verify(r);
 }
