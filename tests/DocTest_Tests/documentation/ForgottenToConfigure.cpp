e6cbee54e3495d04ef596d7845bd986bee01eee5
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-01-14 21:04:19 +0000

    t Add a test that shows output if [Boost].UT doesn't know source location

--- tests/DocTest_Tests/documentation/ForgottenToConfigure.cpp
+++ tests/DocTest_Tests/documentation/ForgottenToConfigure.cpp
@@ -12,3 +12,11 @@ TEST_CASE("HelpMessageForIncorrectBuildConfig")
     Approvals::verify(HelpMessages::getMisconfiguredBuildHelp(
         "../../../tests/Catch2_Tests/ApprovalsTests.cpp"));
 }
+
+TEST_CASE("HelpMessageForIncorrectBoostUTConfig")
+{
+    // This is the error message that is issued if the build
+    // is does not satisfy all the requirements for Boost.UT
+    // with ApprovalTests.cpp
+    Approvals::verify(HelpMessages::getMisconfiguredBuildHelp("unknown"));
+}
