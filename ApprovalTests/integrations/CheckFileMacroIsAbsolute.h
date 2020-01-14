52e613fc45f7593ec5b2b79fe515e011557443d4
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-01-14 19:39:03 +0000

    F Provide option to disable the compile-time check of __FILE__ macro in case there are any build scenarios where __FILE__ is correct but our logic in the check is wrong. See #86

--- ApprovalTests/integrations/CheckFileMacroIsAbsolute.h
+++ ApprovalTests/integrations/CheckFileMacroIsAbsolute.h
@@ -5,6 +5,16 @@
 // not contain any newline characters, as their display looks messy
 // in some compiler outputs.
 // <SingleHpp unalterable>
+//
+// This static_assert can be disabled by
+// defining APPROVALS_CATCH_DISABLE_FILE_MACRO_CHECK.
+// This can be done on the CMake command line with:
+// -DCMAKE_CXX_FLAGS_INIT=-DAPPROVALS_CATCH_DISABLE_FILE_MACRO_CHECK
+//
+// ApprovalTests will then check the validity of __FILE__
+// at run-time instead, for test frameworks that use it to
+// detect the source file name.
+#ifndef APPROVALS_CATCH_DISABLE_FILE_MACRO_CHECK
 #ifdef _WIN32
 static_assert(__FILE__[1] == ':',
 #else
@@ -17,6 +27,7 @@ static_assert(__FILE__[0] == '/',
               // end-snippet
               // clang-format on
 );
+#endif // APPROVALS_CATCH_DISABLE_FILE_MACRO_CHECK
 // </SingleHpp>
 
 #endif //APPROVALTESTS_CPP_CHECKFILEMACROISABSOLUTE_H
