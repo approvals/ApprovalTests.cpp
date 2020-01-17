3e86f899d4f4913438e37ca5b62ada3bc392465f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-01-17 20:43:30 +0000

    F Add options to allow other projects to access our third_party libraries

--- CMake/ApprovalTestsOptions.cmake
+++ CMake/ApprovalTestsOptions.cmake
@@ -2,9 +2,21 @@ option(APPROVAL_TESTS_ENABLE_CODE_COVERAGE "Enable coverage reporting for select
 
 option(APPROVAL_TESTS_BUILD_CMAKE_INTEGRATION_TESTING "Enable tests that check our CMake integration" OFF)
 
-option(APPROVAL_TESTS_BUILD_THIRD_PARTY
-        "Include third_party directory. Note that these are always included if this is the top-level project"
+# Control which of our copies/downloads of third_party libraries are made available
+option(APPROVAL_TESTS_BUILD_THIRD_PARTY_GOOGLE_TEST
+        "Download the Google Test framework"
         OFF)
+option(APPROVAL_TESTS_BUILD_THIRD_PARTY_CATCH2
+        "Include this project's copy of the Catch2 test framework"
+        OFF)
+option(APPROVAL_TESTS_BUILD_THIRD_PARTY_DOCTEST
+        "Include this project's copy of the doctest test framework"
+        OFF)
+option(APPROVAL_TESTS_BUILD_THIRD_PARTY_UT
+        "Include this project's copy of the Boost.UT test framework"
+        OFF)
+
+# Control of what targets are built
 option(APPROVAL_TESTS_BUILD_TESTING
         "Build self-tests. Note that these are always built if this is the top-level project"
         OFF)
