ceff65999399c3dc0168f567b57038d700d72db9
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-01-19 15:54:57 +0000

    c Add some TODOs to the CMake options

--- CMake/ApprovalTestsOptions.cmake
+++ CMake/ApprovalTestsOptions.cmake
@@ -1,8 +1,11 @@
+# TODO Move this to the end
 option(APPROVAL_TESTS_ENABLE_CODE_COVERAGE "Enable coverage reporting for selected tests" OFF)
 
+# TODO Remove this
 option(APPROVAL_TESTS_BUILD_CMAKE_INTEGRATION_TESTING "Enable tests that check our CMake integration" OFF)
 
 # Control which of our copies of third_party libraries are made available
+# TODO Add APPROVAL_TESTS_BUILD_THIRD_PARTY_ALL
 option(APPROVAL_TESTS_BUILD_THIRD_PARTY_CATCH2
         "Include this project's copy of the Catch2 test framework"
         OFF)
@@ -14,6 +17,7 @@ option(APPROVAL_TESTS_BUILD_THIRD_PARTY_UT
         OFF)
 
 # Control of what targets are built
+# TODO Add APPROVAL_TESTS_BUILD_ALL
 option(APPROVAL_TESTS_BUILD_TESTING
         "Build self-tests. Note that these are always built if this is the top-level project"
         OFF)
