6367b1c5b5e958d26d9c532c9bb830efaca8c09d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-01-20 22:40:01 +0000

    B Fix compiler warning so I can update to latest Boost.ut error: definition of implicit copy constructor for 'Blocker' is deprecated because it has a user-declared destructor [-Werror,-Wdeprecated]         virtual ~Blocker() = default;

--- ApprovalTests/utilities/Blocker.h
+++ ApprovalTests/utilities/Blocker.h
@@ -6,7 +6,14 @@ namespace ApprovalTests
     class Blocker
     {
     public:
+        Blocker() = default;
+        Blocker(const Blocker&) = default;
+        Blocker(Blocker&&) = default;
+        Blocker& operator=(const Blocker&) = default;
+        Blocker& operator=(Blocker&&) = default;
+
         virtual ~Blocker() = default;
+
         virtual bool isBlockingOnThisMachine() const = 0;
     };
 }
