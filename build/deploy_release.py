2dc70a5334c374bec3d6b5b2ed43453e980a12bc
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-01-22 20:28:58 +0000

    B Fix URL args
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- build/deploy_release.py
+++ build/deploy_release.py
@@ -47,13 +47,13 @@ class DeployRelease:
         release_notes = read_file(self.details.new_release_notes_path)
         pyperclip.copy(release_notes)
         print('The release notes are on the clipboard')
-        github_url = F"https://github.com/approvals/ApprovalTests.cpp/releases/new?tag={self.details.new_version}&title=Single%20Hpp%20File%20-%20{self.details.new_version}"
+        github_url = F"'https://github.com/approvals/ApprovalTests.cpp/releases/new?tag={self.details.new_version}&title=Single%20Hpp%20File%20-%20{self.details.new_version}'"
         run(["open", github_url])
         run(["open", self.details.release_dir])
         check_step("that the release is published")
 
         # Draft the tweet
-        tweet_text = F"https://twitter.com/intent/tweet?text=%23ApprovalTests.cpp+{self.details.new_version}+released%2C+now+with+___%21%0D%0Ahttps%3A%2F%2Fgithub.com%2Fapprovals%2FApprovalTests.cpp%2Freleases%2Ftag%2F{self.details.new_version}+%0D%0Aor+try+the+starter+project%3A+https%3A%2F%2Fgithub.com%2Fapprovals%2FApprovalTests.cpp.StarterProject%0D%0AThanks+%40LlewellynFalco+%40ClareMacraeUK+%21"
+        tweet_text = F"'https://twitter.com/intent/tweet?text=%23ApprovalTests.cpp+{self.details.new_version}+released%2C+now+with+___%21%0D%0Ahttps%3A%2F%2Fgithub.com%2Fapprovals%2FApprovalTests.cpp%2Freleases%2Ftag%2F{self.details.new_version}+%0D%0Aor+try+the+starter+project%3A+https%3A%2F%2Fgithub.com%2Fapprovals%2FApprovalTests.cpp.StarterProject%0D%0AThanks+%40LlewellynFalco+%40ClareMacraeUK+%21'"
         run(["open", tweet_text])
 
     def push_everything_live(self):
