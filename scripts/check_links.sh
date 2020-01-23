1956f36d2c8ad36f4d6fdbc3e17f4c781b1cd4ab
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-01-23 15:55:19 +0000

    B Fix problem that CI build passed even if broken links were found

---
+++ scripts/check_links.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Current working directory should be the root directory of the repository
+find . -name cmake-build\* -prune -o -name \*.source.md -exec \
+  markdown-link-check --config mlc_config.json --quiet  {} \; 2> error.txt
+
+if [ -e error.txt ] ; then
+  if grep -q "ERROR:" error.txt; then
+    echo "At least one error found!"
+    rm  error.txt
+    exit 113
+  fi
+else
+  echo -e "All good!"
+fi
