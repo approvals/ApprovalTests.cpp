d5a5a5115fa687a8f99a746dfa05808d509d7de3
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-01-23 16:13:11 +0000

    B Remove some incorrect warnings about machine-generated Markdown files

--- fix_markdown.sh
+++ fix_markdown.sh
@@ -4,15 +4,15 @@ find . -name \*.md | grep -v googletest-src | xargs sed -i '' -f fix_markdown.se
 
 echo
 echo "The following files, if any, are missing their 'top' anchor:"
-find . -name \*.md | grep -v googletest-src | grep -v README.md | grep -v README.source.md | xargs grep -L '<a id="top"></a>' | grep -v 'relnotes_'
+find . -name \*.md | grep -v .include.md | grep -v .approved.md | grep -v googletest-src | grep -v README.md | grep -v README.source.md | xargs grep -L '<a id="top"></a>' | grep -v 'relnotes_'
 
 echo
 echo "The following files in doc, if any, are missing the 'Back to User Guide' line at the end:"
-find ./doc -name \*.md | grep -v googletest-src | grep -v README.md | grep -v README.source.md | xargs grep -L 'Back to User Guide'
+find ./doc -name \*.md | grep -v .include.md | grep -v googletest-src | grep -v README.md | grep -v README.source.md | xargs grep -L 'Back to User Guide'
 
 echo
 echo "The following files, if any, need to have relative references converted to absolute ones"
-find . -name \*.source.md | grep -v googletest-src | xargs grep -P '\]\((?!#|http|/|mailto)' | grep -v '[this link is wrong]'
+find . -name \*.source.md | grep -v .include.md | grep -v googletest-src | xargs grep -P '\]\((?!#|http|/|mailto)' | grep -v '[this link is wrong]'
 
 echo
 echo "The following files, if any, are not in the User Guide (doc/README.md)"
