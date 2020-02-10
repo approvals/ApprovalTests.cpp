c71d3b375ee4147142e498a5294000858c60b8d8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-10 23:57:41 +0000

    t Attempt to understand Windows CI failures

--- ApprovalTests/utilities/FileUtils.h
+++ ApprovalTests/utilities/FileUtils.h
@@ -52,6 +52,33 @@ namespace ApprovalTests
             return filePath.substr(found);
         }
 
+        static std::string readFileThrowIfMissing(std::string fileName)
+        {
+            std::ifstream in(fileName.c_str(), std::ios_base::in);
+            if (!in)
+            {
+                throw std::runtime_error("File does not exist: " + fileName);
+            }
+            std::stringstream written;
+            written << in.rdbuf();
+            in.close();
+
+            std::string text = written.str();
+            return text;
+        }
+
+        static std::string readFileReturnEmptyIfMissing(std::string fileName)
+        {
+            if (FileUtils::fileExists(fileName))
+            {
+                return readFileThrowIfMissing(fileName);
+            }
+            else
+            {
+                return std::string();
+            }
+        }
+
         static void writeToFile(const std::string& filePath,
                                 const std::string& content)
         {
--- tests/DocTest_Tests/main.cpp
+++ tests/DocTest_Tests/main.cpp
@@ -6,4 +6,37 @@
 
 using namespace ApprovalTests;
 
+
+#include "core/Reporter.h"
+#include "utilities/FileUtils.h"
+
+#include <iostream>
+
+
+namespace ApprovalTests
+{
+    // A reporter which uses doctest CHECK statement to check the output
+    class DocTestReporter : public Reporter
+    {
+    public:
+        bool report(std::string received, std::string approved) const override
+        {
+            const auto receivedText =
+                FileUtils::readFileReturnEmptyIfMissing(received);
+            const auto approvedText =
+                FileUtils::readFileReturnEmptyIfMissing(approved);
+            // By using CHECK instead of REQUIRE, program flow continues - Catch does not throw an exception
+            // Note that ApprovalTests will later throw an exception though.
+            std::cout << "Checking content of files:" << std::endl;
+            std::cout << "received: " << received << std::endl;
+            std::cout << "approved: " << approved << std::endl;
+            CHECK(receivedText == approvedText);
+            return true;
+        }
+    };
+} // namespace ApprovalTests
+
+auto ciReporterDisposer = CIBuildOnlyReporterUtils::useAsFrontLoadedReporter(
+    std::make_shared<DocTestReporter>());
+
 auto directory = Approvals::useApprovalsSubdirectory("approval_tests");
