a1918771cfb18f55898bafe3d87e26be76614f8c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-10 22:55:16 +0000

    t Test all supported Windows and Linux reporters

--- tests/DocTest_Tests/reporters/CommandLineReporterTests.cpp
+++ tests/DocTest_Tests/reporters/CommandLineReporterTests.cpp
@@ -12,13 +12,31 @@ TEST_CASE("Test Command Lines")
     std::stringstream stream;
     SystemUtils::debugCommandLines().isTest = true;
     std::vector<std::shared_ptr<GenericDiffReporter>> reporters = {
+        // Mac
         std::make_shared<Mac::BeyondCompareReporter>(),
         std::make_shared<Mac::DiffMergeReporter>(),
         std::make_shared<Mac::KaleidoscopeReporter>(),
         std::make_shared<Mac::P4MergeReporter>(),
         std::make_shared<Mac::KDiff3Reporter>(),
         std::make_shared<Mac::TkDiffReporter>(),
-        std::make_shared<Mac::VisualStudioCodeReporter>()
+        std::make_shared<Mac::VisualStudioCodeReporter>(),
+
+        // Windows
+        std::make_shared<Windows::TortoiseTextDiffReporter>(),
+        std::make_shared<Windows::TortoiseImageDiffReporter>(),
+        std::make_shared<Windows::TortoiseGitTextDiffReporter>(),
+        std::make_shared<Windows::TortoiseGitImageDiffReporter>(),
+        std::make_shared<Windows::BeyondCompare3Reporter>(),
+        std::make_shared<Windows::BeyondCompare4Reporter>(),
+        std::make_shared<Windows::WinMergeReporter>(),
+        std::make_shared<Windows::AraxisMergeReporter>(),
+        std::make_shared<Windows::CodeCompareReporter>(),
+        std::make_shared<Windows::KDiff3Reporter>(),
+        std::make_shared<Windows::VisualStudioCodeReporter>(),
+
+        // Linux
+        std::make_shared<Linux::MeldReporter>(),
+        std::make_shared<Linux::KDiff3Reporter>()
     };
     for (const auto& reporter : reporters)
     {
