83d6b00635380fb906e7b20c30ce452c3c9583a8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-11 10:34:05 +0000

    t Speed up tests of CMake targets.
    It turns out that each time we compile a Catch2 main, it adds 10 seconds to
    our GitHub CI builds.
    
    So instead of creating executables to test that we can find our
    third-party libraries via various CMake target names, we now
    create libraries instead, so the compiler has much less work to do.

--- tests/CMake_Tests/catch2_cmake_checks/catch2_cmake_checks.cpp
+++ tests/CMake_Tests/catch2_cmake_checks/catch2_cmake_checks.cpp
@@ -1,2 +1,6 @@
-#define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
+
+void someFunction()
+{
+    CHECK(1 == 1);
+}
--- tests/CMake_Tests/doctest_cmake_checks/doctest_cmake_checks.cpp
+++ tests/CMake_Tests/doctest_cmake_checks/doctest_cmake_checks.cpp
@@ -1,2 +1,6 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest/doctest.h"
+
+void someFunction()
+{
+        CHECK(1 == 1);
+}
