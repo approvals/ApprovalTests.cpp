f934fd6305c81f9688f32cacf8e0c44555fd63a7
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-11 23:46:34 +0000

    e Add reminder about pip install

--- build/prepare_minor_release.py
+++ build/prepare_minor_release.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python3
 
+# pip3 install -r requirements.txt
+
 import version
 from release_details import build
 
 if __name__ == '__main__':
-    build(version.update_minor, deploy = False)
+    build(version.update_minor, deploy=False)
