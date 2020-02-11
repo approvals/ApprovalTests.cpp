e031bb1eb723dd1a3761b0ee89bf9aa1050ef073
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-11 13:03:22 +0000

    t Fixed a windows-only failing test

--- tests/DocTest_Tests/utilities/SystemUtilsTests.cpp
+++ tests/DocTest_Tests/utilities/SystemUtilsTests.cpp
@@ -21,7 +21,7 @@ TEST_CASE("ItCanFixCaseOfFileNameOnWindows")
         // The current implementation doesn't fix the case of folder names, only the file
         // So for now, check that the folder name is still present, but expect it
         // to be lower case
-        CHECK(StringUtils::contains(fixed_filename, "catch2_tests"));
+        CHECK(StringUtils::contains(fixed_filename, "doctest_tests"));
     }
 }
 
