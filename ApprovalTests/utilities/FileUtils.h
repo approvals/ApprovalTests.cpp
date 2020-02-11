7c87bb7e7687d89e6b707d5971f572192df077fc
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-11 00:04:56 +0000

    t Add tests for recent FileUtils additions

--- ApprovalTests/utilities/FileUtils.h
+++ ApprovalTests/utilities/FileUtils.h
@@ -7,6 +7,7 @@
 
 #include <string>
 #include <sys/stat.h>
+#include <sstream>
 #include "ApprovalTests/writers/StringWriter.h"
 
 namespace ApprovalTests
---
+++ tests/DocTest_Tests/utilities/FileUtilsTests.cpp
@@ -0,0 +1,43 @@
+#include "doctest/doctest.h"
+#include "utilities/FileUtils.h"
+#include "namers/ApprovalTestNamer.h"
+#include "Approvals.h"
+#include "reporters/DiffReporter.h"
+
+#include <iostream>
+
+using namespace ApprovalTests;
+
+TEST_CASE("It reads sample_file.txt correctly")
+{
+    ApprovalTestNamer namer;
+    const auto directory = namer.getDirectory();
+    const std::string inputFile = directory + "sample_file.txt";
+
+    Approvals::verify(
+        ApprovalTests::FileUtils::readFileThrowIfMissing(inputFile),
+        DiffReporter());
+}
+
+TEST_CASE("readFileThrowIfMissing() throws if file is missing")
+{
+    bool exceptionThrown = false;
+    try
+    {
+        ApprovalTests::FileUtils::readFileThrowIfMissing(
+            "some file name that does not exist 1234567890.txt");
+    }
+    catch (const std::exception&)
+    {
+        exceptionThrown = true;
+    }
+    REQUIRE(exceptionThrown);
+}
+
+TEST_CASE(
+    "readFileReturnEmptyIfMissing() returns empty string if file is missing")
+{
+    REQUIRE("" ==
+            ApprovalTests::FileUtils::readFileReturnEmptyIfMissing(
+                "some file name that does not exist 1234567890.txt"));
+}
