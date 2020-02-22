d7329c02b64b90f512f1afe56e26287cc29c321c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-22 18:57:28 +0000

    t Remove unnecessary code.
    TextDiffReporter is the default reporter on CI now.

--- tests/DocTest_Tests/main.cpp
+++ tests/DocTest_Tests/main.cpp
@@ -6,13 +6,4 @@
 
 using namespace ApprovalTests;
 
-#include "core/Reporter.h"
-#include "utilities/FileUtils.h"
-#include "reporters/TextDiffReporter.h"
-
-#include <iostream>
-
-auto ciReporterDisposer = CIBuildOnlyReporterUtils::useAsFrontLoadedReporter(
-    std::make_shared<TextDiffReporter>());
-
 auto directory = Approvals::useApprovalsSubdirectory("approval_tests");
