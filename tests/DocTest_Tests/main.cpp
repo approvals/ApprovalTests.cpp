4085b3583ea41b745d2c69f36b68a3757026b745
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-22 18:55:48 +0000

    t Remove unused, experimental code in DocTest_Tests/main.cpp

--- tests/DocTest_Tests/main.cpp
+++ tests/DocTest_Tests/main.cpp
@@ -12,41 +12,6 @@ using namespace ApprovalTests;
 
 #include <iostream>
 
-namespace ApprovalTests
-{
-    // A reporter which uses doctest CHECK statement to check the output
-    class DocTestReporter : public Reporter
-    {
-    public:
-        bool report(std::string received, std::string approved) const override
-        {
-            const auto receivedText =
-                FileUtils::readFileReturnEmptyIfMissing(received);
-            const auto approvedText =
-                FileUtils::readFileReturnEmptyIfMissing(approved);
-            // By using CHECK instead of REQUIRE, program flow continues - Catch does not throw an exception
-            // Note that ApprovalTests will later throw an exception though.
-            std::cout << "Checking content of files:" << std::endl;
-            std::cout << "received: " << received << std::endl;
-            std::cout
-                << "********************************************************\n"
-                << receivedText << std::endl;
-            std::cout
-                << "********************************************************\n"
-                << std::endl;
-            std::cout << "approved: " << approved << std::endl;
-            std::cout
-                << "********************************************************\n"
-                << approvedText << std::endl;
-            std::cout
-                << "********************************************************\n"
-                << std::endl;
-            //            CHECK(receivedText == approvedText);
-            return true;
-        }
-    };
-} // namespace ApprovalTests
-
 auto ciReporterDisposer = CIBuildOnlyReporterUtils::useAsFrontLoadedReporter(
     std::make_shared<TextDiffReporter>());
 
