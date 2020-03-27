4072067ba3075f75fe27420b00f59bfb8c924dd0
Piotr Podsiadły <ppodsiadly@mykolab.com>
2020-03-27 19:50:24 +0100

    F Mention APPROVALS_DOCTEST_EXISTING_MAIN is docs

--- examples/doctest_existing_main/main.cpp
+++ examples/doctest_existing_main/main.cpp
@@ -1,5 +1,8 @@
+// begin-snippet: doctest_existing_main
+// Add these two lines to the top of your main.cpp file:
 #define APPROVALS_DOCTEST_EXISTING_MAIN
 #include "ApprovalTests.hpp"
+// end-snippet
 
 int main(int argc, char** argv)
 {
