bd3e801b7f40f48e6aa3f93fb3936cadcc489233
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-03-27 21:37:58 +0000

    t Add tests for version Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

---
+++ build/tests/test_version.py
@@ -0,0 +1,24 @@
+import unittest
+
+from scripts.version import load_version, get_version_text, get_version_without_v, update_minor, update_patch, \
+    update_major
+
+
+class TestVersion(unittest.TestCase):
+    def test_starting_point(self):
+        version = load_version()
+        self.assertEqual('1.1.1', get_version_without_v(get_version_text(version)))
+
+    def test_updates(self):
+        self.assert_version('v.1.1.2', update_patch)
+        self.assert_version('v.1.2.0', update_minor)
+        self.assert_version('v.2.0.0', update_major)
+
+    def assert_version(self, expected_version, update_method):
+        version = load_version()
+        version = update_method(version)
+        self.assertEqual(expected_version, get_version_text(version))
+
+
+if __name__ == '__main__':
+    unittest.main()
