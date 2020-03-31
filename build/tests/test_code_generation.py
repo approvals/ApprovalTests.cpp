9ec45856ae06f77ddcc7a71933507927dee2176d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-03-31 18:55:09 +0100

    t Created some code generation tools Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

---
+++ build/tests/test_code_generation.py
@@ -0,0 +1,72 @@
+import unittest
+
+
+class CodeGeneration:
+    @staticmethod
+    def convert_string_to_concatenation(content):
+        lines = content.splitlines()
+        code = '('
+        for line in lines:
+            if '{' in line:
+                code += 'f'
+            code += f"'{line}\\n'\n"
+        code += ')'
+        return code
+
+    @staticmethod
+    def convert_string_to_expected(content):
+        lines = content.splitlines()
+        code = "'\\n'.join([\n"
+        for line in lines:
+            if '{' in line:
+                code += 'f'
+            code += f"'{line}',\n"
+        code += '])'
+        return code
+
+
+class TestCodeGeneration(unittest.TestCase):
+    def test_convert_string_to_concatentation(self):
+        content = '''
+toc
+
+## v.x.y.z
+
+{self.old_feature_text()}
+
+'''
+        result = CodeGeneration.convert_string_to_concatenation(content)
+        expected = r"""('\n'
+'toc\n'
+'\n'
+'## v.x.y.z\n'
+'\n'
+f'{self.old_feature_text()}\n'
+'\n'
+)"""
+        self.assertEqual(expected, result)
+
+    def test_convert_string_to_joined_list(self):
+        content = '''
+toc
+
+## v.x.y.z
+
+{self.old_feature_text()}
+
+'''
+        result = CodeGeneration.convert_string_to_expected(content)
+        expected = r"""'\n'.join([
+'',
+'toc',
+'',
+'## v.x.y.z',
+'',
+f'{self.old_feature_text()}',
+'',
+])"""
+        self.assertEqual(expected, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
