4329d6c524acbae837551b1268a7c7d1d317c099
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-04-01 22:31:56 +0100

    e Rework create_simulated_single_header.sh to call create_simulated_single_header_file.py (We could eventually delete create_simulated_single_header.sh - but for now it is still referred to in its output file, ApprovalTests/ApprovalTests.hpp

--- scripts/create_simulated_single_header.sh
+++ scripts/create_simulated_single_header.sh
@@ -5,22 +5,8 @@
 # just as users of the released single-header would do.
 #
 # Run this after adding new header files (*.h) in ApprovalTests
+#
+# It is also run as part of the release process.
 
-output_file='ApprovalTests/ApprovalTests.hpp'
-
-cat << HERE > $output_file
-#ifndef APPROVALTESTS_CPP_APPROVALS_HPP
-#define APPROVALTESTS_CPP_APPROVALS_HPP
-
-// Created with:
-//   cd ApprovalTests.cpp
-//   ./scripts/create_simulated_single_header.sh
-
-HERE
-
-find ApprovalTests -depth -name \*.h | sed -e 's/^/#include "/' | sed -e 's/$/"/' | sort >> $output_file
-
-cat << HERE >> $output_file
-
-#endif // APPROVALTESTS_CPP_APPROVALS_HPP
-HERE
+cd build
+./create_simulated_single_header_file.py
