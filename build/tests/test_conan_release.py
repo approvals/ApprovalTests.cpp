41da42cbcba7d66e10710c077f00c266572b11c0
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-04-02 16:27:26 +0100

    t Enable all Python tests to run in PyCharm

--- build/tests/test_conan_release.py
+++ build/tests/test_conan_release.py
@@ -2,12 +2,16 @@ import os
 import unittest
 
 from scripts.conan_release import PrepareConanRelease
+from scripts.utilities import get_file_name
 
 
 def set_home_directory():
-    print(os.getcwd())
-    os.chdir("../../ApprovalTests")
-    print(os.getcwd())
+    current_dir = os.getcwd().replace('\\', '/')
+    if current_dir.endswith('ApprovalTests.cpp/ApprovalTests'):
+        return
+    source_dir = os.path.split(__file__)[0]
+    library_dir = os.path.join(source_dir, "../../ApprovalTests")
+    os.chdir(library_dir)
 
 
 class TestConanRelease(unittest.TestCase):
--- build/tests/test_version.py
+++ build/tests/test_version.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from scripts.version import load_version, get_version_text, get_version_without_v, update_minor, update_patch, \
@@ -6,7 +7,7 @@ from scripts.version import load_version, get_version_text, get_version_without_
 
 class TestVersion(unittest.TestCase):
     def test_starting_point(self):
-        version = load_version('.')
+        version = self.load_test_version_ini()
         self.assertEqual('1.1.1', get_version_without_v(get_version_text(version)))
 
     def test_create_version(self):
@@ -19,8 +20,14 @@ class TestVersion(unittest.TestCase):
         self.assert_version('v.1.2.0', update_minor)
         self.assert_version('v.2.0.0', update_major)
 
+    @staticmethod
+    def load_test_version_ini():
+        source_dir = os.path.split(__file__)[0]
+        version = load_version(source_dir)
+        return version
+
     def assert_version(self, expected_version, update_method):
-        version = load_version('.')
+        version = self.load_test_version_ini()
         version = update_method(version)
         self.assertEqual(expected_version, get_version_text(version))
 
