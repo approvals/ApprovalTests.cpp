e5c841ff153e599eb2660ba56dc1e6bce12866f4
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-04-02 16:15:13 +0100

    e Simplify comments

--- scripts/create_simulated_single_header.sh
+++ scripts/create_simulated_single_header.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-# This script regenerates the file allows
-# development builds to include 'ApprovalTests.hpp',
-# just as users of the released single-header would do.
-#
-# Run this after adding new header files (*.h) in ApprovalTests
-#
-# It is also run as part of the release process.
+# This script regenerates ApprovalTests/ApprovalTests.hpp
 
 cd build
 ./create_simulated_single_header_file.py
