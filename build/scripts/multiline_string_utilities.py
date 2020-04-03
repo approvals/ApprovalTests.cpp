680e30cc94417fe1d8b8caaba95b06c18a976961
Llewellyn Falco <isidore@setgame.com>
2020-04-03 12:05:05 -0700

    t still playing with multiline strings

--- build/scripts/multiline_string_utilities.py
+++ build/scripts/multiline_string_utilities.py
@@ -1,11 +1,16 @@
 import textwrap
 
 
+def remove_indentation_from(text):
+    cleaned = textwrap.dedent(text)
+    if cleaned.startswith("\n"):
+        cleaned = cleaned[1:]
+    return cleaned
+
+
 class RemoveMultilineIndentation:
     def __lshift__(self, text):
-        cleaned = textwrap.dedent(text)
-        if cleaned.startswith("\n"):
-            cleaned = cleaned[1:]
-        return cleaned
+        return remove_indentation_from(text)
+
 
 remove_indentation = RemoveMultilineIndentation()
