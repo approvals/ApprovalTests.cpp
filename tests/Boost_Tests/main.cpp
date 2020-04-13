97c8ade222c1f34d7937340ac80a504401056223
Llewellyn Falco <isidore@setgame.com>
2020-04-13 10:01:19 -0700

    t docs
    Co-Authored-By: Clare Macrae <github@cfmacrae.fastmail.co.uk>

--- tests/Boost_Tests/main.cpp
+++ tests/Boost_Tests/main.cpp
@@ -1,7 +1,7 @@
 #define BOOST_TEST_MODULE ModuleName
 
-//#include <boost/test/unit_test.hpp>
-#include <boost/test/included/unit_test.hpp>
+//#include <boost/test/unit_test.hpp> // static or dynamic boost build
+#include <boost/test/included/unit_test.hpp> // header only boost
 
 // begin-snippet: boost_test_main
 // main.cpp (after #including boost.test)
