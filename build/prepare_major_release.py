047336813aa9e3f6ec47d3a0d6b1f5e9cf50c10f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-04-17 21:45:23 +0100

    R!! Fix lambdas
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- build/prepare_major_release.py
+++ build/prepare_major_release.py
@@ -4,4 +4,4 @@ from scripts import version
 from scripts.prepare_release import build
 
 if __name__ == '__main__':
-    build(lambda v: v.update_major, deploy = False)
+    build(lambda v: v.update_major(), deploy = False)
