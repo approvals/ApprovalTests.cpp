e3446d7c55788df718697fc6fb77f9b94b9b466c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-04-18 15:13:32 +0100

    e Make check_type_hints.sh warn about missing type annotations

--- build/check_type_hints.sh
+++ build/check_type_hints.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
+#  --disallow-untyped-calls  Disallow calling functions without type
+#                            annotations from functions with type annotations
+#                            (inverse: --allow-untyped-calls)
+#  --disallow-untyped-defs   Disallow defining functions without type
+#                            annotations or with incomplete type annotations
+#                            (inverse: --allow-untyped-defs)
+#  --disallow-incomplete-defs
+#                            Disallow defining functions with incomplete type
+#                            annotations (inverse: --allow-incomplete-defs)
+
 python3 -m mypy \
   --follow-imports=skip \
   --ignore-missing-imports \
+  --disallow-untyped-calls \
+  --disallow-untyped-defs \
+  --disallow-incomplete-defs \
   *.py \
   scripts/*.py \
   tests/*.py
