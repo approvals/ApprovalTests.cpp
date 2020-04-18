72090a4980ae9307107699413d2dc3ab9d2b81c2
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-04-18 15:08:10 +0100

    e Fixed add_type_hints.sh for much better coverage

--- build/add_type_hints.sh
+++ build/add_type_hints.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+# Make an edit to doc/mdsource/Features.source.md, to cover more code...
+
 # https://instagram-engineering.com/let-your-code-type-hint-itself-introducing-open-source-monkeytype-a855c7284881
 rm -f monkeytype.sqlite3
-monkeytype run *.py scripts/*.py
+monkeytype run -m unittest tests/*.py
+monkeytype run prepare_major_release.py
 for module in $(monkeytype list-modules) ; do
+  echo updating $module
   monkeytype apply $module
 done
