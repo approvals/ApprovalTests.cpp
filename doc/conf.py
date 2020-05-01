9abc78e8bca333e2068186fd0ef567e79e2cba03
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-05-01 12:13:47 +0100

    e Enable building our documentation on ReadTheDocs. (#129)

--- doc/conf.py
+++ doc/conf.py
@@ -50,3 +50,31 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Teach RtD to use CMake config  ------------------------------------------
+
+import subprocess, os
+
+
+def configureDoxyfile(input_dir, output_dir):
+    with open('Doxyfile.in', 'r') as file:
+        filedata = file.read()
+
+    filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
+    filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
+
+    with open('Doxyfile', 'w') as file:
+        file.write(filedata)
+
+
+# Check if we're running on Read the Docs' servers
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+breathe_projects = {}
+
+if read_the_docs_build:
+    input_dir = '../ApprovalTests'
+    output_dir = 'build'
+    configureDoxyfile(input_dir, output_dir)
+    subprocess.call('doxygen', shell=True)
+    breathe_projects['ApprovalTests.cpp'] = output_dir + '/xml'
