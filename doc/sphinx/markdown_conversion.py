f0dac5fa5769bd879f3ebc119e0ac9c1366f94e0
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-05-03 15:49:31 +0100

    r Extract method

--- doc/sphinx/markdown_conversion.py
+++ doc/sphinx/markdown_conversion.py
@@ -10,7 +10,10 @@ def convertMarkdownDocsToRst():
     input_dir = '../../doc'
     output_dir = 'generated_docs'
     file_base_name = 'UsingCatch'
+    convert_markdown_to_restructured_text(file_base_name, input_dir, output_dir)
+
+
+def convert_markdown_to_restructured_text(file_base_name, input_dir, output_dir):
     output = pypandoc.convert_file(f'{input_dir}/{file_base_name}.md', 'rst',
                                    outputfile=f'{output_dir}/{file_base_name}.rst')
-
     assert output == ""
