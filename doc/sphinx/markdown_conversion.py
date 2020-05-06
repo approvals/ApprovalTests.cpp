21d5a9c832b1b982dc6066b98dabd500d7ecffb8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-05-06 22:12:05 +0100

    t Tests no longer save the processed .md files

--- doc/sphinx/markdown_conversion.py
+++ doc/sphinx/markdown_conversion.py
@@ -45,8 +45,8 @@ def convert_markdown_file_to_restructured_text(subdir, file_base_name, input_dir
     converted_markdown, converted_rst = convert_markdown_text_to_restructured_text(content, subdir)
 
     # Temporary code for reviewing changes made, on all input files:
-    with open(file_base_name + '_hacked.md', 'w') as w:
-        w.write(converted_markdown)
+    # with open(file_base_name + '_hacked.md', 'w') as w:
+    #     w.write(converted_markdown)
 
     with open(f'{output_dir}/{file_base_name}.rst', 'w') as rst_output:
         rst_output.write(converted_rst)
