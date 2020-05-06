8046ff7af0fb7b6ff23f6134819b4422d02260ce
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-05-06 14:00:06 +0100

    t Reinstate some manual tests of links to various relative paths

--- doc/sphinx/tests/test_markdown_conversion.py
+++ doc/sphinx/tests/test_markdown_conversion.py
@@ -5,6 +5,29 @@ from approvaltests.reporters.generic_diff_reporter_factory import GenericDiffRep
 from doc.sphinx import markdown_conversion
 
 
+class HyperlinkUpdatingTestCase(unittest.TestCase):
+    def test_file_in_subdir_links_to_file_in_dir_above(self):
+        self.check(
+            r'See [Target 1](/doc/Doc1.md#target1).',
+            r'See [Target 1](../Doc1.html#target1).',
+            'subdir')
+
+    def test_file_in_parent_dir_links_to_file_in_subdir(self):
+        self.check(
+            r'See [Target 1](/doc/subdir/Doc1.md#target1).',
+            r'See [Target 1](subdir/Doc1.html#target1).')
+
+    def test_file_in_one_subdir_links_to_file_in_different_subdir(self):
+        self.check(
+            r'See [Target 1](/doc/subdir1/Doc1.md#target1).',
+            r'See [Target 1](../subdir1/Doc1.html#target1).',
+            'subdir2')
+
+    def check(self, content, expected, subdir=''):
+        actual = markdown_conversion.fixup_markdown_hyperlinks(content, subdir, 'SomeDocFile')
+        self.assertEqual(expected, actual)
+
+
 class TestWholeConversion(unittest.TestCase):
     def test_convert_github_markdown_for_input_to_pandoc_in_root_docdir(self) -> None:
         with open('test_markdown_conversion_input.md') as f:
