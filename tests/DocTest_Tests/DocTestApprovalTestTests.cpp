1263c106a16204a865a4827414f1ed7f8f38a47d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-05-07 21:52:49 +0100

    t Add new snippet for docs, specify_all_the_options
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- tests/DocTest_Tests/DocTestApprovalTestTests.cpp
+++ tests/DocTest_Tests/DocTestApprovalTestTests.cpp
@@ -1,3 +1,4 @@
+#include "ApprovalTests/reporters/QuietReporter.h"
 #include "doctest/doctest.h"
 #include "ApprovalTests/Approvals.h"
 #include "ApprovalTests/writers/StringWriter.h"
@@ -32,4 +33,23 @@ TEST_CASE("YouCanSpecifyYourFileExtensionWithFormatter")
         1337,
         [](auto value, auto& os) { os << "**value:** " << value; },
         Options().fileOptions().withFileExtension(".md"));
+}
+
+TEST_CASE("YouCanSpecifyAllOptions")
+{
+    Approvals::verify(
+        R"(
+{
+    name: frances
+    id: 05f77de3-3790-4d45-b045-def96c9cd371
+}
+          )",
+        // begin-snippet: specify_all_the_options
+        Options()
+            .withReporter(QuietReporter())
+            .withScrubber(Scrubbers::scrubGuid)
+            .fileOptions()
+            .withFileExtension(".json")
+        // end-snippet
+    );
 }
