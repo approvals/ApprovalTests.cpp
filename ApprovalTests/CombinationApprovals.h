339414cf0db9622583e7922574c46643e0b81fc8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-05-11 21:42:37 +0100

    R Fix formatting and make some helpers private
    Co-Authored-By: Alessandro Pezzato <alepez@users.noreply.github.com>
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- ApprovalTests/CombinationApprovals.h
+++ ApprovalTests/CombinationApprovals.h
@@ -14,39 +14,39 @@ namespace ApprovalTests
 {
     template <typename TCompileTimeOptions> class TCombinationApprovals
     {
+    private:
+        // Write out second or subsequent input value, with preceding comma and space
+        struct print_input
+        {
+            std::ostream& out;
+            template <class T> void operator()(const T& input)
+            {
+                out << ", " << TCompileTimeOptions::ToStringConverter::toString(input);
+            }
+        };
+
+        // Write out one row of output
+        template <class Converter> struct serialize
+        {
+            std::ostream& out;
+            Converter converter;
+            template <class T, class... Ts> void operator()(T&& input1_, Ts&&... inputs)
+            {
+                // First value is printed without trailing comma
+                out << "(" << TCompileTimeOptions::ToStringConverter::toString(input1_);
+                // Remaining values are printed with prefix of a comma
+                CartesianProduct::Detail::for_each(std::forward_as_tuple(inputs...),
+                                                   print_input{out});
+                out << ") => " << converter(input1_, inputs...) << '\n';
+            }
+        };
+
     public:
-            // Write out second or subsequent input value, with preceding comma and space
-            struct print_input
-            {
-                std::ostream& out;
-                template <class T> void operator()(const T& input)
-                {
-                    out << ", " << TCompileTimeOptions::ToStringConverter::toString(input);
-                }
-            };
-
-            // Write out one row of output
-            template <class Converter> struct serialize
-            {
-                std::ostream& out;
-                Converter converter;
-                template <class T, class... Ts>
-                void operator()(T&& input1_, Ts&&... inputs)
-                {
-                    // First value is printed without trailing comma
-                    out << "(" << TCompileTimeOptions::ToStringConverter::toString(input1_);
-                    // Remaining values are printed with prefix of a comma
-                    CartesianProduct::Detail::for_each(std::forward_as_tuple(inputs...),
-                                                       print_input{out});
-                    out << ") => " << converter(input1_, inputs...) << '\n';
-                }
-            };
-
         template <class Converter, class Container, class... Containers>
         static void verifyAllCombinations(const Options& options,
-                                   Converter&& converter,
-                                   const Container& input0,
-                                   const Containers&... inputs)
+                                          Converter&& converter,
+                                          const Container& input0,
+                                          const Containers&... inputs)
         {
             std::stringstream s;
             CartesianProduct::cartesian_product(
@@ -79,7 +79,7 @@ namespace ApprovalTests
         }
     };
 
-    using CombinationApprovals =
-    TCombinationApprovals<ToStringCompileTimeOptions<APPROVAL_TESTS_DEFAULT_STREAM_CONVERTER>>;
+    using CombinationApprovals = TCombinationApprovals<
+        ToStringCompileTimeOptions<APPROVAL_TESTS_DEFAULT_STREAM_CONVERTER>>;
 
 } // namespace ApprovalTests
