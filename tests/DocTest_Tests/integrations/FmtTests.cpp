56eac6f08ec69c00a4a4cbb3aa1b03f87bd33765
Llewellyn Falco <isidore@setgame.com>
2020-05-18 14:45:39 -0700

    r format
    Co-Authored-By: Alessandro Pezzato <alepez@users.noreply.github.com>

--- tests/DocTest_Tests/integrations/FmtTests.cpp
+++ tests/DocTest_Tests/integrations/FmtTests.cpp
@@ -10,13 +10,10 @@
 
 using namespace ApprovalTests;
 
-
 TEST_CASE("FmtContainers")
 {
     // begin-snippet: fmt_approvals
-    std::vector<int> numbers = {1,2,3};
+    std::vector<int> numbers = {1, 2, 3};
     FmtApprovals::verify(numbers);
     // end-snippet
 }
-
-
