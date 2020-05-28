e1d052adb2b51cd0485551a36b050b3435664acb
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-05-28 17:44:28 +0100

    e Enable releasing from both Llewellyn and my Macs
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- build/scripts/conan_release.py
+++ build/scripts/conan_release.py
@@ -11,9 +11,19 @@ from scripts.version import Version
 
 class ConanReleaseDetails:
     def __init__(self) -> None:
-        # self.conan_repo_dir = '../../conan-center-index-claremacrae' # llewellyn
-        self.conan_repo_dir = '../../../conan/conan-center-index-claremacrae' # clare
+        self.conan_repo_dir = ConanReleaseDetails.get_conan_repo_directory()
         self.conan_approvaltests_dir = os.path.join(self.conan_repo_dir, 'recipes', 'approvaltests.cpp')
+
+    @staticmethod
+    def get_conan_repo_directory() -> str:
+        possibles = [
+            '../../conan-center-index-claremacrae',  # llewellyn
+            '../../../conan/conan-center-index-claremacrae'  # clare
+        ]
+        for dir in possibles:
+            if os.path.isdir(dir):
+                return dir
+        raise RuntimeError("Could not find fork of conan-center-index")
 
 
 class PrepareConanRelease:
--- build/tests/test_conan_release.py
+++ build/tests/test_conan_release.py
@@ -2,7 +2,7 @@ import unittest
 
 from approvaltests.approvals import verify
 
-from scripts.conan_release import PrepareConanRelease
+from scripts.conan_release import PrepareConanRelease, ConanReleaseDetails
 from scripts.version import Version
 from tests.helpers import set_home_directory
 
@@ -24,3 +24,10 @@ class TestConanRelease(unittest.TestCase):
             text += PrepareConanRelease.create_conandata_yml_text(Version(1, 1, i), "single_header_sha",
                                                                   "licence_sha")
         verify(text)
+
+    def test_can_find_conan_repo(self) -> None:
+        set_home_directory()
+        # Validate that function does not throw exception.
+        # Will fail if person running test has not cloned conan-center-index
+        directory = ConanReleaseDetails.get_conan_repo_directory()
+        print(directory)
