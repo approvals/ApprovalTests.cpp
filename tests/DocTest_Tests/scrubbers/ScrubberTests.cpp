d78f3fe36ac9d6e606a841cc7d4b28ea9cc2a732
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-01 18:07:01 +0100

    t Tidy up some test code
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- tests/DocTest_Tests/scrubbers/ScrubberTests.cpp
+++ tests/DocTest_Tests/scrubbers/ScrubberTests.cpp
@@ -28,24 +28,11 @@ TEST_CASE("test createRegexScrubber with string input and fixed result")
     Approvals::verify(input, Options(Scrubbers::createRegexScrubber(R"(\d+)", "number")));
 }
 
-//TEST_CASE("regex scrubber with Options")
-//{
-//    auto input = "Hello 1234 World";
-//    auto output = Scrubbers::createRegexScrubber("1234", "number");
-//    auto output = Scrubbers::createRegexScrubber("1234", [](const std::string& /*match*/){ return "number";});
-//    auto output = [](auto text){
-//        return Scrubbers::scrubRegex(text, "1234", [](const std::string&){ return "number";})
-//    };
-//    REQUIRE(output == "guid_1 and normal text");
-//    Approvals::verify(input, Options(function));
-//}
-
 TEST_CASE("regex scrubber")
 {
     auto input = "9012 Hello 1234 World 1234 5678";
     auto scrubbed =
         Scrubbers::scrubRegex(input, std::regex(R"(\d\d\d\d)"), [](const auto& match) {
-            std::cout << match.str() << '\n';
             return "number(" + match.str() + ")\n";
         });
     Approvals::verify(scrubbed);
