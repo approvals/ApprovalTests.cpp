9de50fe8b6a297591bb5ddf8888644ba3aa2fdf3
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-04 20:41:03 +0100

    R Remove APPROVAL_TESTS_HIDE_DEPRECATED_CODE
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- ApprovalTests/ApprovalsMacroDefaults.h
+++ ApprovalTests/ApprovalsMacroDefaults.h
@@ -1,7 +1,3 @@
 #pragma once
 
-// clang-format off
-#if ! defined(APPROVAL_TESTS_HIDE_DEPRECATED_CODE)
-#define       APPROVAL_TESTS_HIDE_DEPRECATED_CODE 1
-#endif
-// clang-format on
+// This file intentionally left blank.
