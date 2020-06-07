1bca7f0fb03b34597b6e2c2331399bcab14529b3
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-07 18:15:35 +0100

    R!! release_constants.py now gets some info from project_details.py
    ApprovalTests.Cpp has changed to ApprovalTests.cpp, as shown by
    the changed TestReleaseConstants.test.approved.txt
    
    The folder names on github have .cpp, now .Cpp, so this should
    be fine.

--- build/scripts/project_details.py
+++ build/scripts/project_details.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class ProjectDetails:
     github_project_name: str = 'ApprovalTests.cpp'
     library_folder_name: str = 'ApprovalTests'
+    simulated_single_header_file: str = 'ApprovalTests.hpp'
     version_header: str = 'ApprovalTestsVersion.h'
     macro_prefix: str = 'APPROVAL_TESTS'
     conan_directory_name: str = 'approvaltests.cpp'
--- build/scripts/release_constants.py
+++ build/scripts/release_constants.py
@@ -1,21 +1,23 @@
 import os
 
+from scripts.project_details import ProjectDetails
+
 
 class ReleaseConstants:
-    def __init__(self) -> None:
+    def __init__(self, project_details: ProjectDetails) -> None:
 
-        self.approval_tests_dir = F"../ApprovalTests"
+        self.approval_tests_dir = F"../{project_details.library_folder_name}"
         self.build_dir = F"../build"
         self.release_dir = F"../build/releases"
 
-        self.main_project_dir = F"../../ApprovalTests.Cpp"
-        self.starter_project_dir = F"../../ApprovalTests.Cpp.StarterProject"
+        self.main_project_dir = F"../../{project_details.github_project_name}"
+        self.starter_project_dir = F"../../{project_details.github_project_name}.StarterProject"
 
-        self.simulated_single_header_file_path = os.path.join(self.approval_tests_dir, "ApprovalTests.hpp")
+        self.simulated_single_header_file_path = os.path.join(self.approval_tests_dir, project_details.simulated_single_header_file)
 
         self.xxx_release_notes_path = os.path.join(self.build_dir, F'relnotes_x.y.z.md')
         self.release_notes_dir = os.path.join(self.build_dir, 'release_notes')
         self.template_release_notes_path = os.path.join(self.release_notes_dir, F'relnotes_template.md')
 
 
-release_constants = ReleaseConstants()
+release_constants = ReleaseConstants(ProjectDetails())
