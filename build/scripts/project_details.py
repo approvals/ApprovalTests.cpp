1bb5c542052cc43afd876333f9e8e1f96d1f46d4
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-08 09:03:09 +0100

    e Add some github URLs to ProjectDetails

--- build/scripts/project_details.py
+++ build/scripts/project_details.py
@@ -4,6 +4,8 @@ from dataclasses import dataclass
 @dataclass
 class ProjectDetails:
     github_project_name: str = 'ApprovalTests.cpp'
+    github_project_raw_content_url: str = f"https://raw.githubusercontent.com/approvals/{github_project_name}"
+    github_project_url: str = f"https://github.com/approvals/{github_project_name}"
     library_folder_name: str = 'ApprovalTests'
     simulated_single_header_file: str = 'ApprovalTests.hpp'
     version_header: str = 'ApprovalTestsVersion.h'
