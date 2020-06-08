318ef1200b9fc5a0ecde2ff321d29ce6b7fc6778
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-08 08:06:59 +0100

    r Reorder data members in ReleaseDetails

--- build/scripts/release_details.py
+++ build/scripts/release_details.py
@@ -8,10 +8,11 @@ from scripts.version import Version
 class ReleaseDetails:
     def __init__(self, old_version: Version, new_version: Version, publish_release: bool,
                  project_details: ProjectDetails) -> None:
+        self.project_details = project_details
+
         self.old_version = old_version
         self.new_version = new_version
         self.push_to_production = publish_release
-        self.project_details = project_details
 
         self.old_single_header = F"ApprovalTests.{old_version.get_version_text()}.hpp"
         self.new_single_header = F"ApprovalTests.{new_version.get_version_text()}.hpp"
