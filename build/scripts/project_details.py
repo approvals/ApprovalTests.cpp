f57983ea3e6dae043dafd01a9ab650ca37638a94
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-08 11:47:36 +0100

    R Convert two URLs in ProjectDetails to properties
    This is to reduce the amount of duplicated code needed in the
    ApprovalTests.cpp.Qt project

--- build/scripts/project_details.py
+++ build/scripts/project_details.py
@@ -4,8 +4,6 @@ from dataclasses import dataclass
 @dataclass
 class ProjectDetails:
     github_project_name: str = 'ApprovalTests.cpp'
-    github_project_raw_content_url: str = f"https://raw.githubusercontent.com/approvals/{github_project_name}"
-    github_project_url: str = f"https://github.com/approvals/{github_project_name}"
     library_folder_name: str = 'ApprovalTests'
     simulated_single_header_file: str = 'ApprovalTests.hpp'
     version_header: str = 'ApprovalTestsVersion.h'
@@ -14,4 +12,12 @@ class ProjectDetails:
     update_documentation: bool = True
     update_conan: bool = True
 
-# The __init__() is generated automatically
+    # The __init__() is generated automatically
+
+    @property
+    def github_project_raw_content_url(self) -> str:
+        return f"https://raw.githubusercontent.com/approvals/{self.github_project_name}"
+
+    @property
+    def github_project_url(self) -> str:
+        return f"https://github.com/approvals/{self.github_project_name}"
