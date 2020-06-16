8576360b688983aa56ad79617de453c3e0379ba5
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-16 20:28:58 +0100

    R   Removing another hard-coded 'approvaltests.cpp' reference

--- build/scripts/conan_release_details.py
+++ build/scripts/conan_release_details.py
@@ -6,7 +6,8 @@ from scripts.project_details import ProjectDetails
 class ConanReleaseDetails:
     def __init__(self, project_details: ProjectDetails) -> None:
         self.conan_repo_dir = ConanReleaseDetails.get_conan_repo_directory()
-        self.conan_approvaltests_dir = os.path.join(self.conan_repo_dir, 'recipes', 'approvaltests.cpp')
+        self.conan_approvaltests_dir = os.path.join(self.conan_repo_dir, 'recipes',
+                                                    project_details.conan_directory_name)
 
     @staticmethod
     def get_conan_repo_directory() -> str:
