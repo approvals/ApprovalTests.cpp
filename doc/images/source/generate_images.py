aa59fc15f36f6e171577628ceca966aa2d0ad85a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-07-01 10:07:39 +0100

    e   Make output less verbose & more readable

--- doc/images/source/generate_images.py
+++ doc/images/source/generate_images.py
@@ -4,11 +4,10 @@ import subprocess
 from glob import glob
 
 for input_file in glob('*.dot'):
-    print(input_file)
     output_format = 'png'
     base_name = os.path.splitext(input_file)[0]
     output_file = os.path.join('..', base_name + '.' + output_format)
 
     command = f'dot -T{output_format} {input_file} -o {output_file}'
-    print(command)
     subprocess.run(command, shell=True, check=True)
+    print(f'regenerated {output_file}')
