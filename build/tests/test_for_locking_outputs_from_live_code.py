474a0e482118e97559d6c1e4a6301a5a6b3c5c03
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-07-10 13:02:21 +0100

    t   Turn on locking tests temporarily

--- build/tests/test_for_locking_outputs_from_live_code.py
+++ build/tests/test_for_locking_outputs_from_live_code.py
@@ -12,7 +12,7 @@ from tests.helpers import set_home_directory
 
 class TestForLocking(unittest.TestCase):
 
-    def disabled_test_create_single_header_file_approvals(self) -> None:
+    def test_create_single_header_file_approvals(self) -> None:
         prepare_release = self.get_prepare_release()
         output = prepare_release.create_single_header_file()
 
@@ -20,13 +20,13 @@ class TestForLocking(unittest.TestCase):
         # over time. It is here to help when refactoring the release process.
         verify_file(output)
 
-    def disabled_test_create_simulated_single_header_file(self) -> None:
+    def test_create_simulated_single_header_file(self) -> None:
         # The output of this depends on the current C++ code, so changes
         # over time. It is here to help when refactoring the release process.
         prepare_release = self.get_prepare_release()
         verify(SingleHeaderFile.create_content('.', prepare_release.details.project_details, include_cpps=False))
 
-    def disabled_test_create_simulated_single_header_file_with_cpps(self) -> None:
+    def test_create_simulated_single_header_file_with_cpps(self) -> None:
         # The output of this depends on the current C++ code, so changes
         # over time. It is here to help when refactoring the release process.
         prepare_release = self.get_prepare_release()
