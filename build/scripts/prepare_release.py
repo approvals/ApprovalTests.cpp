5dd371fa80f4261fece18c3a5e67f8af003414ae
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-07-18 19:29:07 +0100

    r   Extract methods from class

--- build/scripts/prepare_release.py
+++ build/scripts/prepare_release.py
@@ -24,7 +24,7 @@ class PrepareRelease:
     def check_pre_conditions_for_publish(self) -> None:
         if self.details.push_to_production:
             self.check_pre_conditions_for_main_repo()
-            self.check_pre_conditions_for_starter_project_repo()
+            check_pre_conditions_for_starter_project_repo(self.details)
 
             run(["open", F"{self.details.project_details.github_project_url}/commits/master"])
             check_step("the builds are passing")
@@ -50,57 +50,6 @@ class PrepareRelease:
         assert_step(len(
             list(repo.iter_commits('master@{u}..master'))) == 0,
                     f"there are un-pushed changes in {self.details.project_details.github_project_name}")
-
-    def check_pre_conditions_for_starter_project_repo(self) -> None:
-        repo = Repo(self.details.locations.starter_project_dir)
-        assert_step(not repo.bare)
-        GitUtilities.check_branch_name(repo, 'master')
-
-    def update_starter_project(self) -> None:
-        STARTER_PATH_OLD_SINGLE_HEADER = F"{self.details.locations.starter_project_dir}/lib/{self.details.old_single_header}"
-        STARTER_PATH_NEW_SINGLE_HEADER = F"{self.details.locations.starter_project_dir}/lib/{self.details.new_single_header}"
-
-        # Make sure starter project folder is clean
-        project_dir = self.details.locations.starter_project_dir
-        GitUtilities.reset_and_clean_working_directory(project_dir)
-
-        shutil.copyfile(self.details.release_new_single_header, STARTER_PATH_NEW_SINGLE_HEADER)
-
-        # Delete the last release:
-        if os.path.exists(STARTER_PATH_OLD_SINGLE_HEADER):
-            os.remove(STARTER_PATH_OLD_SINGLE_HEADER)
-        else:
-            raise RuntimeError(F"""
-----------------------------------------------------------------
-ERROR: Old header file does not exist:
-{STARTER_PATH_OLD_SINGLE_HEADER}
-Starting state of Starter Project does not match '{self.details.old_version.get_version_text()}'
-Check whether:
-1. There were uncommitted changes to version.ini in main project,
-   from a previous release preparation step.
-2. The Starter Project repo needs pulling.
-----------------------------------------------------------------
-
-""")
-
-        # Update the version in the "redirect" header:
-        replace_text_in_file(
-            F"{self.details.locations.starter_project_dir}/lib/{self.details.project_details.simulated_single_header_file}",
-            self.details.old_version.get_version_text(),
-            self.details.new_version.get_version_text())
-
-        # Update the version number in the Visual Studio project:
-        visual_studio_2017_sln = F"{self.details.locations.starter_project_dir}/visual-studio-2017/StarterProject.vcxproj"
-        if os.path.isfile(visual_studio_2017_sln):
-            replace_text_in_file(visual_studio_2017_sln,
-                                 self.details.old_single_header,
-                                 self.details.new_single_header)
-        else:
-            print(f"Info: No Visual Studio solution file: {visual_studio_2017_sln}")
-
-    def check_starter_project_builds(self) -> None:
-        with use_directory(F"{self.details.locations.starter_project_dir}/cmake-build-debug"):
-            run(["cmake", "--build", "."])
 
     def add_to_git(self) -> None:
         def add() -> None:
@@ -133,8 +82,8 @@ Check whether:
 
         CppGeneration.prepare_release(self.details)
 
-        self.update_starter_project()
-        self.check_starter_project_builds()
+        update_starter_project(self.details)
+        check_starter_project_builds(self.details)
 
         PrepareConanRelease.prepare_release(self.details)
 
@@ -144,6 +93,60 @@ Check whether:
         self.add_to_git()
 
         self.check_changes()
+
+
+def check_pre_conditions_for_starter_project_repo(details: ReleaseDetails) -> None:
+    repo = Repo(details.locations.starter_project_dir)
+    assert_step(not repo.bare)
+    GitUtilities.check_branch_name(repo, 'master')
+
+
+def update_starter_project(details: ReleaseDetails) -> None:
+    STARTER_PATH_OLD_SINGLE_HEADER = F"{details.locations.starter_project_dir}/lib/{details.old_single_header}"
+    STARTER_PATH_NEW_SINGLE_HEADER = F"{details.locations.starter_project_dir}/lib/{details.new_single_header}"
+
+    # Make sure starter project folder is clean
+    project_dir = details.locations.starter_project_dir
+    GitUtilities.reset_and_clean_working_directory(project_dir)
+
+    shutil.copyfile(details.release_new_single_header, STARTER_PATH_NEW_SINGLE_HEADER)
+
+    # Delete the last release:
+    if os.path.exists(STARTER_PATH_OLD_SINGLE_HEADER):
+        os.remove(STARTER_PATH_OLD_SINGLE_HEADER)
+    else:
+        raise RuntimeError(F"""
+----------------------------------------------------------------
+ERROR: Old header file does not exist:
+{STARTER_PATH_OLD_SINGLE_HEADER}
+Starting state of Starter Project does not match '{details.old_version.get_version_text()}'
+Check whether:
+1. There were uncommitted changes to version.ini in main project,
+from a previous release preparation step.
+2. The Starter Project repo needs pulling.
+----------------------------------------------------------------
+
+""")
+
+    # Update the version in the "redirect" header:
+    replace_text_in_file(
+        F"{details.locations.starter_project_dir}/lib/{details.project_details.simulated_single_header_file}",
+        details.old_version.get_version_text(),
+        details.new_version.get_version_text())
+
+    # Update the version number in the Visual Studio project:
+    visual_studio_2017_sln = F"{details.locations.starter_project_dir}/visual-studio-2017/StarterProject.vcxproj"
+    if os.path.isfile(visual_studio_2017_sln):
+        replace_text_in_file(visual_studio_2017_sln,
+                             details.old_single_header,
+                             details.new_single_header)
+    else:
+        print(f"Info: No Visual Studio solution file: {visual_studio_2017_sln}")
+
+
+def check_starter_project_builds(details: ReleaseDetails) -> None:
+    with use_directory(F"{details.locations.starter_project_dir}/cmake-build-debug"):
+        run(["cmake", "--build", "."])
 
 
 def build(update_version: Callable[[Version], Version], deploy: bool,
