24e0863a383b33be32f3755e6044ed7523ee285b
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-08-06 13:21:37 +0100

    t   Demo some DateTime formatting

---
+++ tests/DocTest_Tests/docs/DateTimeScrubbingSamples.cpp
@@ -0,0 +1,49 @@
+#include "doctest/doctest.h"
+
+#include "utilities/DateUtils.h"
+#include "Approvals.h"
+
+using namespace ApprovalTests;
+
+TEST_CASE("Test DateTime scrubbing - with default format")
+{
+    // Thu 2020-08-06 11:28:33 UTC
+    std::string textWithDate =
+        "date: " + DateUtils::toString(std::chrono::system_clock::now());
+    std::string day = R"([A-Za-z]{3})";
+
+    std::string year = R"(\d\d\d\d)";
+    std::string month = R"(\d\d)";
+    std::string date = R"(\d\d)";
+    std::string time = R"(\d\d:\d\d:\d\d)";
+    std::string time_zone = "UTC";
+
+    const auto dateRegex =
+        day + " " + year + "-" + month + "-" + date + " " + time + " " + time_zone;
+
+    auto scrubber = Scrubbers::createRegexScrubber(dateRegex, "[date_and_time]");
+    CHECK("date: [date_and_time]" == scrubber(textWithDate));
+
+    Approvals::verify(textWithDate, Options().withScrubber(scrubber));
+}
+
+TEST_CASE("Test DateTime scrubbing - with specific format")
+{
+    // Mon Jun 22 14:07:34 2020
+    // %a  %b  %e %H:%M:%S %G
+    std::string textWithDate =
+        "date: " +
+        DateUtils::toString(std::chrono::system_clock::now(), "%a %b %e %H:%M:%S %G");
+
+    std::string day = R"([A-Za-z]{3})";
+    std::string month = R"([A-Za-z]{3})";
+    std::string date = R"([\d ]\d)";
+    std::string time = R"(\d\d:\d\d:\d\d)";
+    std::string year = R"(\d\d\d\d)";
+    const auto dateRegex = day + " " + month + " " + date + " " + time + " " + year;
+
+    auto scrubber = Scrubbers::createRegexScrubber(dateRegex, "[date_and_time]");
+    CHECK("date: [date_and_time]" == scrubber(textWithDate));
+
+    Approvals::verify(textWithDate, Options().withScrubber(scrubber));
+}
