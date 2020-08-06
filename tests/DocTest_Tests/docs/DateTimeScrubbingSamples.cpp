3817553368cb48b7a3ace24e747109f1e1b9f9ea
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-08-06 14:15:05 +0100

    r   Extract variable

--- tests/DocTest_Tests/docs/DateTimeScrubbingSamples.cpp
+++ tests/DocTest_Tests/docs/DateTimeScrubbingSamples.cpp
@@ -28,9 +28,8 @@ TEST_CASE("Test DateTime scrubbing - with specific format")
 {
     // Mon Jun 22 14:07:34 2020
     // %a  %b  %e %H:%M:%S %G
-    std::string textWithDate =
-        "date: " +
-        DateUtils::toString(std::chrono::system_clock::now(), "%a %b %e %T %G");
+    const std::chrono::time_point dateTime = std::chrono::system_clock::now();
+    std::string textWithDate = "date: " + DateUtils::toString(dateTime, "%a %b %e %T %G");
 
     std::string weekDay = R"([A-Za-z]{3})";
     std::string month = R"([A-Za-z]{3})";
