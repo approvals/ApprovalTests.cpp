634eb53b81fbca9c329fa1c89bc7104fbcfd11c0
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-08-06 13:29:23 +0100

    t   Rename variable

--- tests/DocTest_Tests/docs/DateTimeScrubbingSamples.cpp
+++ tests/DocTest_Tests/docs/DateTimeScrubbingSamples.cpp
@@ -10,7 +10,7 @@ TEST_CASE("Test DateTime scrubbing - with default format")
     // Thu 2020-08-06 11:28:33 UTC
     std::string textWithDate =
         "date: " + DateUtils::toString(std::chrono::system_clock::now());
-    std::string day = R"([A-Za-z]{3})";
+    std::string weekDay = R"([A-Za-z]{3})";
 
     std::string year = R"(\d\d\d\d)";
     std::string month = R"(\d\d)";
@@ -19,7 +19,7 @@ TEST_CASE("Test DateTime scrubbing - with default format")
     std::string time_zone = "UTC";
 
     const auto dateRegex =
-        day + " " + year + "-" + month + "-" + date + " " + time + " " + time_zone;
+        weekDay + " " + year + "-" + month + "-" + date + " " + time + " " + time_zone;
 
     auto scrubber = Scrubbers::createRegexScrubber(dateRegex, "[date_and_time]");
     CHECK("date: [date_and_time]" == scrubber(textWithDate));
@@ -35,12 +35,12 @@ TEST_CASE("Test DateTime scrubbing - with specific format")
         "date: " +
         DateUtils::toString(std::chrono::system_clock::now(), "%a %b %e %T %G");
 
-    std::string day = R"([A-Za-z]{3})";
+    std::string weekDay = R"([A-Za-z]{3})";
     std::string month = R"([A-Za-z]{3})";
     std::string date = R"([\d ]\d)";
     std::string time = R"(\d\d:\d\d:\d\d)";
     std::string year = R"(\d\d\d\d)";
-    const auto dateRegex = day + " " + month + " " + date + " " + time + " " + year;
+    const auto dateRegex = weekDay + " " + month + " " + date + " " + time + " " + year;
 
     auto scrubber = Scrubbers::createRegexScrubber(dateRegex, "[date_and_time]");
     CHECK("date: [date_and_time]" == scrubber(textWithDate));
