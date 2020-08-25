fb33b7cf5ea3549a6fc7e1301a429ea9ee78e1ef
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-08-25 22:51:01 +0100

    F   Add support for AutoApproveReporter
    Co-Authored-By: Alessandro Pezzato <alepez@users.noreply.github.com>
    Co-Authored-By: Andrea Beducci <andrea@beducci.eu>

--- ApprovalTests/reporters/ReporterFactory.cpp
+++ ApprovalTests/reporters/ReporterFactory.cpp
@@ -1,8 +1,12 @@
 #include "ReporterFactory.h"
+#include "ApprovalTests/reporters/AutoApproveReporter.h"
 #include "ApprovalTests/reporters/ClipboardReporter.h"
 
 #include <map>
 #include <functional>
+
+#define APPROVAL_TESTS_REGISTER_REPORTER(name)                                                          \
+    map[#name] = []() { return std::unique_ptr<Reporter>(new name); };
 
 namespace ApprovalTests
 {
@@ -10,9 +14,9 @@ namespace ApprovalTests
     ReporterFactory::createReporter(const std::string& reporterName)
     {
         std::map<std::string, std::function<std::unique_ptr<Reporter>()>> map;
-        map["ClipboardReporter"] = []() {
-            return std::unique_ptr<Reporter>(new ClipboardReporter);
-        };
+
+        APPROVAL_TESTS_REGISTER_REPORTER(AutoApproveReporter);
+        APPROVAL_TESTS_REGISTER_REPORTER(ClipboardReporter);
 
         auto iter = map.find(reporterName);
         if (iter != map.end())
