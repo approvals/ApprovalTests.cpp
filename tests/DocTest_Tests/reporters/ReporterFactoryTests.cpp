d115964286fc1a6886829330009174879ef6f561
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2020-09-08 22:54:59 +0200

    t   Simplify test code

--- tests/DocTest_Tests/reporters/ReporterFactoryTests.cpp
+++ tests/DocTest_Tests/reporters/ReporterFactoryTests.cpp
@@ -10,39 +10,34 @@ using namespace ApprovalTests;
 TEST_CASE("ReporterFactory creates a Reporter given a string")
 {
     ReporterFactory factory;
-    std::unique_ptr<Reporter> reporter = factory.createReporter("ClipboardReporter");
-    CHECK(reporter);
+    auto reporter = factory.createReporter("ClipboardReporter");
     CHECK(dynamic_cast<ClipboardReporter*>(reporter.get()));
 }
 
 TEST_CASE("ReporterFactory returns null if reporter is unknown")
 {
     ReporterFactory factory;
-    std::unique_ptr<Reporter> reporter = factory.createReporter("IDoNotExist");
+    auto reporter = factory.createReporter("IDoNotExist");
     CHECK(!reporter);
 }
 
 TEST_CASE("ReporterFactory creates a Reporter given Linux::MeldReporter")
 {
     ReporterFactory factory;
-    std::unique_ptr<Reporter> reporter = factory.createReporter("Linux::MeldReporter");
-    CHECK(reporter);
+    auto reporter = factory.createReporter("Linux::MeldReporter");
     CHECK(dynamic_cast<Linux::MeldReporter*>(reporter.get()));
 }
 
 TEST_CASE("ReporterFactory creates a Reporter given Windows::WinMergeReporter")
 {
     ReporterFactory factory;
-    std::unique_ptr<Reporter> reporter =
-        factory.createReporter("Windows::WinMergeReporter");
-    CHECK(reporter);
+    auto reporter = factory.createReporter("Windows::WinMergeReporter");
     CHECK(dynamic_cast<Windows::WinMergeReporter*>(reporter.get()));
 }
 
 TEST_CASE("ReporterFactory creates a Reporter given Windows::WinMerge")
 {
     ReporterFactory factory;
-    std::unique_ptr<Reporter> reporter = factory.createReporter("Windows::WinMerge");
-    CHECK(reporter);
+    auto reporter = factory.createReporter("Windows::WinMerge");
     CHECK(dynamic_cast<Windows::WinMergeReporter*>(reporter.get()));
 }
