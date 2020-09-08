d35913f85c41fe3a9ed60b0571924594bf3caf09
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2020-09-08 22:32:01 +0200

    F   Add windows, linux, mac reporters (#99)

--- tests/DocTest_Tests/reporters/ReporterFactoryTests.cpp
+++ tests/DocTest_Tests/reporters/ReporterFactoryTests.cpp
@@ -1,5 +1,7 @@
 #include "ApprovalTests/reporters/ReporterFactory.h"
 #include "ApprovalTests/reporters/ClipboardReporter.h"
+#include "ApprovalTests/reporters/LinuxReporters.h"
+#include "ApprovalTests/reporters/WindowsReporters.h"
 
 #include "doctest/doctest.h"
 
@@ -18,4 +20,21 @@ TEST_CASE("ReporterFactory returns null if reporter is unknown")
     ReporterFactory factory;
     std::unique_ptr<Reporter> reporter = factory.createReporter("IDoNotExist");
     CHECK(!reporter);
+}
+
+TEST_CASE("ReporterFactory creates a Reporter given Linux::MeldReporter")
+{
+    ReporterFactory factory;
+    std::unique_ptr<Reporter> reporter = factory.createReporter("Linux::MeldReporter");
+    CHECK(reporter);
+    CHECK(dynamic_cast<Linux::MeldReporter*>(reporter.get()));
+}
+
+TEST_CASE("ReporterFactory creates a Reporter given Windows::WinMergeReporter")
+{
+    ReporterFactory factory;
+    std::unique_ptr<Reporter> reporter =
+        factory.createReporter("Windows::WinMergeReporter");
+    CHECK(reporter);
+    CHECK(dynamic_cast<Windows::WinMergeReporter*>(reporter.get()));
 }
