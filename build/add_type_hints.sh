bbe41baa0af187fb2d560db0b851cc2d03440936
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-10-01 12:11:54 +0100

    e Change all mdsource/blah.source.md to blah.md
    See #158

--- build/add_type_hints.sh
+++ build/add_type_hints.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Make an edit to doc/mdsource/Features.source.md, to cover more code...
+# Make an edit to doc/Features.md, to cover more code...
 
 # https://instagram-engineering.com/let-your-code-type-hint-itself-introducing-open-source-monkeytype-a855c7284881
 rm -f monkeytype.sqlite3
