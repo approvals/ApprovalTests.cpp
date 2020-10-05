7e77280fb17377b786a5c78170030f5ff31c3e1d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-10-05 22:15:29 +0100

    e   Remove more spurious warnings from fix_markdown.sh

--- fix_markdown.sh
+++ fix_markdown.sh
@@ -4,7 +4,7 @@ find . -name \*.md | grep -v googletest-src | xargs sed -i '' -f fix_markdown.se
 
 echo
 echo "The following files, if any, are missing their 'top' anchor:"
-find . -name \*.md | grep -v .include.md | grep -v .approved.md | grep -v googletest-src | grep -v README.md | xargs grep -L '<a id="top"></a>' | grep -v 'relnotes_'
+find . -name \*.md | grep -v .include.md | grep -v .approved.md | grep -v googletest-src | grep -v README.md | grep -v cmake-build- | grep -v '/venv/' | xargs grep -L '<a id="top"></a>' | grep -v 'relnotes_'
 
 echo
 echo "The following files in doc, if any, are missing the 'Back to User Guide' line at the end:"
