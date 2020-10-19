ebf8c1ca3bb53bf5b1119f53beecc81862c7c77c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-10-19 13:20:15 +0100

    e   Moved the advanced CMake options to end of ApprovalTestsOptions.cmake

--- CMake/ApprovalTestsOptions.cmake
+++ CMake/ApprovalTestsOptions.cmake
@@ -12,6 +12,31 @@ option(APPROVAL_TESTS_BUILD_DOCS
         ${NOT_SUBPROJECT})
 
 # -------------------------------------------------------------------
+# Control which of our copies of header-only third_party libraries are made available.
+# These only affect builds where ApprovalTests.cpp is a sub-project.
+# All the third-party libraries are made available
+# if this is the top-level project, so that we can run our tests.
+option(APPROVAL_TESTS_BUILD_THIRD_PARTY_CATCH2
+        "Include this project's copy of the Catch2 test framework"
+        ${NOT_SUBPROJECT})
+option(APPROVAL_TESTS_BUILD_THIRD_PARTY_DOCTEST
+        "Include this project's copy of the doctest test framework"
+        ${NOT_SUBPROJECT})
+option(APPROVAL_TESTS_BUILD_THIRD_PARTY_UT
+        "Include this project's copy of the Boost.UT test framework"
+        ${NOT_SUBPROJECT})
+
+# -------------------------------------------------------------------
+# Options to control the behaviour of our builds.
+option(APPROVAL_TESTS_ENABLE_CODE_COVERAGE
+        "Enable coverage reporting for selected tests"
+        OFF)
+
+# -------------------------------------------------------------------
+# Advanced options - which are not documented.
+# These interact in complication ways with the existing CMake options,
+# and documenting these interactions would clutter the docs
+# considerably.
 option(APPROVAL_TESTS_ENABLE_BOOST_TEST_INTEGRATION
         "Enable the Boost.Test integration and testing"
         ON)
@@ -45,24 +70,3 @@ option(APPROVAL_TESTS_ENABLE_UT_INTEGRATION
         "Enable the Boost.UT integration and testing"
         ON)
 mark_as_advanced(APPROVAL_TESTS_ENABLE_UT_INTEGRATION)
-
-# -------------------------------------------------------------------
-# Control which of our copies of header-only third_party libraries are made available.
-# These only affect builds where ApprovalTests.cpp is a sub-project.
-# All the third-party libraries are made available
-# if this is the top-level project, so that we can run our tests.
-option(APPROVAL_TESTS_BUILD_THIRD_PARTY_CATCH2
-        "Include this project's copy of the Catch2 test framework"
-        ${NOT_SUBPROJECT})
-option(APPROVAL_TESTS_BUILD_THIRD_PARTY_DOCTEST
-        "Include this project's copy of the doctest test framework"
-        ${NOT_SUBPROJECT})
-option(APPROVAL_TESTS_BUILD_THIRD_PARTY_UT
-        "Include this project's copy of the Boost.UT test framework"
-        ${NOT_SUBPROJECT})
-
-# -------------------------------------------------------------------
-# Options to control the behaviour of our builds.
-option(APPROVAL_TESTS_ENABLE_CODE_COVERAGE
-        "Enable coverage reporting for selected tests"
-        OFF)
