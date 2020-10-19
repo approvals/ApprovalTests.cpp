828a9ad764d5851b41ab20c2ef29a154caa6c439
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-10-19 12:35:08 +0100

    e   Add APPROVAL_TESTS_ENABLE_CATCH2_INTEGRATION CMake option
    It's on by default.

--- CMake/ApprovalTestsOptions.cmake
+++ CMake/ApprovalTestsOptions.cmake
@@ -12,6 +12,10 @@ option(APPROVAL_TESTS_BUILD_DOCS
         ${NOT_SUBPROJECT})
 
 # -------------------------------------------------------------------
+option(APPROVAL_TESTS_ENABLE_CATCH2_INTEGRATION
+        "Enable the Catch2 integration and testing"
+        ON)
+
 option(APPROVAL_TESTS_ENABLE_CPPUTEST_INTEGRATION
         "Enable the CppUTest integration and testing"
         ON)
