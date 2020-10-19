46139503659ed7cd93a6a09d9b03bede5b034d38
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-10-19 11:59:38 +0100

    e   Simplify CMake logic

--- CMake/ApprovalTestsOptions.cmake
+++ CMake/ApprovalTestsOptions.cmake
@@ -3,13 +3,13 @@
 # Note that these are always built if this is the top-level project.
 option(APPROVAL_TESTS_BUILD_TESTING
         "Build self-tests."
-        OFF)
+        ${NOT_SUBPROJECT})
 option(APPROVAL_TESTS_BUILD_EXAMPLES
         "Build documentation examples."
-        OFF)
+        ${NOT_SUBPROJECT})
 option(APPROVAL_TESTS_BUILD_DOCS
         "Build documentation - if Doxygen and Sphinx are found."
-        OFF)
+        ${NOT_SUBPROJECT})
 
 # -------------------------------------------------------------------
 # Control which of our copies of header-only third_party libraries are made available.
@@ -18,13 +18,13 @@ option(APPROVAL_TESTS_BUILD_DOCS
 # if this is the top-level project, so that we can run our tests.
 option(APPROVAL_TESTS_BUILD_THIRD_PARTY_CATCH2
         "Include this project's copy of the Catch2 test framework"
-        OFF)
+        ${NOT_SUBPROJECT})
 option(APPROVAL_TESTS_BUILD_THIRD_PARTY_DOCTEST
         "Include this project's copy of the doctest test framework"
-        OFF)
+        ${NOT_SUBPROJECT})
 option(APPROVAL_TESTS_BUILD_THIRD_PARTY_UT
         "Include this project's copy of the Boost.UT test framework"
-        OFF)
+        ${NOT_SUBPROJECT})
 
 # -------------------------------------------------------------------
 # Options to control the behaviour of our builds.
