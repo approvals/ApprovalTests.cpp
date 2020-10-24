c9bae72b305f85e236c4fce69ec94c3894ab64c0
ClareMacrae <github@cfmacrae.fastmail.co.uk>
2020-10-24 15:30:47 +0100

    e   Move code for installing Python requirements to install_python_requirements.sh
    This will allow me to test it on an Ubuntu VM

--- build/install_python_requirements.sh
+++ build/install_python_requirements.sh
@@ -2,5 +2,9 @@
 
 # To update, use:
 #   pip3 install --upgrade --no-cache-dir
+sudo apt-get install pandoc
+pandoc --version
+python3 -m pip install --upgrade pip
+pip3 install flake8 pytest
 pip3 install -r requirements.txt
 pip3 install -r ../doc/requirements.txt
