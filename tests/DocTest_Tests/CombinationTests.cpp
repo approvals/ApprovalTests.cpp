7f0b6d1ae161b5b56ca5b5195933ee9ca5030092
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-11-16 17:29:47 +0000

    r   Reformat code
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- tests/DocTest_Tests/CombinationTests.cpp
+++ tests/DocTest_Tests/CombinationTests.cpp
@@ -66,7 +66,9 @@ TEST_CASE("CombinationsStartingPoint")
     std::vector<std::string> inputs1{"input1.value1", "input1.value2"};
     std::vector<std::string> inputs2{"input2.value1", "input2.value2", "input2.value3"};
     CombinationApprovals::verifyAllCombinations(
-        [&](auto /*input1*/, auto /*input2*/) { return "placeholder"; }, inputs1, inputs2);
+        [&](auto /*input1*/, auto /*input2*/) { return "placeholder"; },
+        inputs1,
+        inputs2);
 }
 // end-snippet
 
