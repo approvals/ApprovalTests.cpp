a4e0a5f9e015c0b85b7075cb32779b95b1341c05
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-01-09 14:56:38 +0000

    e   Add a script for checking out the dependencies of this project
    This is intended for use with:
    https://github.com/claremacrae/ApprovalTests.cpp.CMakeSamples/tree/main/dev_approvals

---
+++ scripts/clone_dependencies.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Force execution to halt if there are any errors in this script:
+set -e
+set -o pipefail
+
+# Boost needs to be installed or cloned separately
+git clone https://github.com/abseil/googletest.git
+git clone https://github.com/boost-ext/ut.git
+git clone https://github.com/catchorg/Catch2.git -b v2.x --single-branch
+git clone https://github.com/cpputest/cpputest.git
+git clone https://github.com/fmtlib/fmt.git
+git clone https://github.com/onqtam/doctest.git
