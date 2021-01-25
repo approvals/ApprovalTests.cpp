d0b12bd24bfbd481a5999bcd9dbfec142d5330c8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-01-25 17:52:36 +0000

    e   Speed up fix_markdown.sh - which is really slow if lots of build dirs
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- fix_markdown.sh
+++ fix_markdown.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-find . -name \*.md | grep -v googletest-src | xargs sed -i '' -f fix_markdown.sed
+find ./doc -name \*.md | grep -v googletest-src | xargs sed -i '' -f fix_markdown.sed
 
 echo
 echo "The following files, if any, are missing their 'top' anchor:"
-find . -name \*.md | grep -v .include.md | grep -v .approved.md | grep -v googletest-src | grep -v README.md | grep -v cmake-build- | grep -v '/venv/' | xargs grep -L '<a id="top"></a>' | grep -v 'relnotes_'
+find ./doc -name \*.md | grep -v .include.md | grep -v .approved.md | grep -v googletest-src | grep -v README.md | grep -v cmake-build- | grep -v '/venv/' | xargs grep -L '<a id="top"></a>' | grep -v 'relnotes_'
 
 echo
 echo "The following files in doc, if any, are missing the 'Back to User Guide' line at the end:"
