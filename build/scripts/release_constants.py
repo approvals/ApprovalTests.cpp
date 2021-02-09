6d382a87fbc802b3c08143057560e511f1bb60d6
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-02-09 15:27:49 +0000

    e   Create the releases directory, if it does not exist

--- build/scripts/release_constants.py
+++ build/scripts/release_constants.py
@@ -5,6 +5,8 @@ class ReleaseConstants:
     def __init__(self) -> None:
         self.build_dir = F"../build"
         self.release_dir = F"../build/releases"
+        if not os.path.isdir(self.release_dir):
+            os.mkdir(self.release_dir)
 
         self.xxx_release_notes_path = os.path.join(self.build_dir, F'relnotes_x.y.z.md')
         self.release_notes_dir = os.path.join(self.build_dir, 'release_notes')
