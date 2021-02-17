87ce0612d8a78d590c86924c9d27f61d33eee2da
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-02-17 21:35:55 +0000

    t   Add ut_starter, for use in documentation

---
+++ examples/ut_starter/ut_starter_test.cpp
@@ -0,0 +1,20 @@
+#define APPROVALS_UT
+#include "ApprovalTests.hpp"
+
+int main()
+{
+    using namespace boost::ut;
+    using namespace ApprovalTests;
+
+    // This puts "received" and "approved" files in approval_tests/ sub-directory,
+    // keeping the test source directory tidy:
+    auto directory = Approvals::useApprovalsSubdirectory("approval_tests");
+
+    "Starter"_test = []() {
+        // TODO Replace 42 with the value or object whose contents you are verifying.
+        // For help, see:
+        // https://approvaltestscpp.readthedocs.io/en/latest/generated_docs/ToString.html
+        Approvals::verify(42);
+    };
+
+}
