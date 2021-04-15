67ae088cb7d45128f6936fedd19735e9c6bd8a98
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-04-15 16:47:49 +0100

    t Add new class CustomNamer and tests for it
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

---
+++ tests/DocTest_Tests/namers/CustomNamerTests.cpp
@@ -0,0 +1,57 @@
+#include "doctest/doctest.h"
+#include "ApprovalTests/core/ApprovalNamer.h"
+#include "ApprovalTests/namers/SeparateApprovedAndReceivedDirectoriesNamer.h"
+#include "ApprovalTests/namers/NamerFactory.h"
+#include "ApprovalTests/utilities/SystemUtils.h"
+
+//#include <filesystem>
+#include <iostream>
+#include <memory>
+
+using namespace ApprovalTests;
+
+class CustomNamer : public ApprovalTestNamer
+{
+
+public:
+    std::string getTestFolder() const
+    {
+        return getTestSourceDirectory();
+    }
+
+    std::string getTestFolderForApproved() const
+    {
+        return getTestFolder();
+    }
+
+    std::string getRelativePathOfSourceDirectoryFromSourceRootForApproved() const
+    {
+        return ApprovalTestNamer::
+            getRelativePathOfSourceDirectoryFromSourceRootForApproved();
+    }
+
+    std::string getFileNameAndTestName() const
+    {
+        return getFileName() + "." + getTestName();
+    }
+
+    virtual std::string getApprovedFile(std::string extensionWithDot) const override
+    {
+        return getTestFolderForApproved() + "/" +
+               getRelativePathOfSourceDirectoryFromSourceRootForApproved() + "/" +
+               getFileNameAndTestName() + ".approved" + extensionWithDot;
+    }
+
+    virtual std::string getReceivedFile(std::string /*extensionWithDot*/) const override
+    {
+        return "my.received";
+    }
+};
+
+TEST_CASE("Default Behaviour")
+{
+    auto result = Approvals::getDefaultNamer()->getApprovedFile(".txt");
+    auto custom = CustomNamer().getApprovedFile(".txt");
+    //Approvals::verify(custom);
+    REQUIRE(result == custom);
+}
