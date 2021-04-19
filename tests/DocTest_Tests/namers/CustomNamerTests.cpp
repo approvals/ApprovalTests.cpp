50f59660e601699efc721484011505babb27d4b1
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-04-19 19:04:03 +0100

    t Add test for injecting TemplatedCustomNamer
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- tests/DocTest_Tests/namers/CustomNamerTests.cpp
+++ tests/DocTest_Tests/namers/CustomNamerTests.cpp
@@ -1,5 +1,6 @@
 #include "doctest/doctest.h"
 #include "ApprovalTests/namers/TemplatedCustomNamer.h"
+#include "ApprovalTests/Approvals.h"
 
 using namespace ApprovalTests;
 
@@ -14,6 +15,20 @@ TEST_CASE("Test StringTemplates")
           "/my/source/directory/approved/CustomNamerTests.Test_StringTemplates.txt");
     CHECK(namer.getReceivedFile(".txt") ==
           "/my/source/directory/received/CustomNamerTests.Test_StringTemplates.txt");
+}
+
+TEST_CASE("Test Namer Injection")
+{
+    // begin-snippet: templated_custom_namer_injection
+    auto default_namer_disposer = Approvals::useAsDefaultNamer(
+        []()
+        {
+            return std::make_shared<TemplatedCustomNamer>(
+                "{TestSourceDirectory}/CustomName.{ApprovedOrReceived}.{FileExtension}");
+        });
+    // end-snippet
+
+    Approvals::verify("Hello");
 }
 
 // TODO Move Path impl to .cpp
