21ef4c24a73cb28a6b2cf0c09b61f9cc520c682f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-04-19 18:20:38 +0100

    r Extract variables
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- ApprovalTests/namers/TemplatedCustomNamer.cpp
+++ ApprovalTests/namers/TemplatedCustomNamer.cpp
@@ -8,19 +8,26 @@ namespace ApprovalTests
     {
     }
     std::string TemplatedCustomNamer::constructFromTemplate(
-        const std::string& extensionWithDot, const std::string& approvedOrReceived) const
+        const std::string& extensionWithDot,
+        const std::string& approvedOrReceivedReplacement) const
     {
         std::string result = template_;
+        auto fileExtension = "{FileExtension}";
+        auto testFileName = "{TestFileName}";
+        auto testCaseName = "{TestCaseName}";
+        auto testSourceDirectory = "{TestSourceDirectory}";
+        auto approvedOrReceived = "{ApprovedOrReceived}";
+
         result = ApprovalTests::StringUtils::replaceAll(
-            result, "{FileExtension}", extensionWithDot.substr(1));
+            result, fileExtension, extensionWithDot.substr(1));
         result = ApprovalTests::StringUtils::replaceAll(
-            result, "{TestFileName}", namer_.getSourceFileName());
+            result, testFileName, namer_.getSourceFileName());
         result = ApprovalTests::StringUtils::replaceAll(
-            result, "{TestCaseName}", namer_.getTestName());
+            result, testCaseName, namer_.getTestName());
         result = ApprovalTests::StringUtils::replaceAll(
-            result, "{TestSourceDirectory}", namer_.getDirectory());
+            result, testSourceDirectory, namer_.getDirectory());
         result = ApprovalTests::StringUtils::replaceAll(
-            result, "{ApprovedOrReceived}", approvedOrReceived);
+            result, approvedOrReceived, approvedOrReceivedReplacement);
         return result;
     }
 
