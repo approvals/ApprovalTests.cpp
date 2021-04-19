09569b5f3cdb9c210892300a28ac050832f9c306
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-04-19 18:57:12 +0100

    t Delete obsolete code
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- tests/DocTest_Tests/namers/CustomNamerTests.cpp
+++ tests/DocTest_Tests/namers/CustomNamerTests.cpp
@@ -1,12 +1,5 @@
 #include "doctest/doctest.h"
-#include "ApprovalTests/core/ApprovalNamer.h"
-#include "ApprovalTests/namers/SeparateApprovedAndReceivedDirectoriesNamer.h"
-#include "ApprovalTests/namers/NamerFactory.h"
 #include "ApprovalTests/namers/TemplatedCustomNamer.h"
-#include "ApprovalTests/utilities/Path.h"
-#include "ApprovalTests/utilities/SystemUtils.h"
-
-#include <utility>
 
 using namespace ApprovalTests;
 
@@ -23,14 +16,6 @@ TEST_CASE("Test StringTemplates")
           "/my/source/directory/received/CustomNamerTests.Test_StringTemplates.txt");
 }
 
-// TODO Better names for methods
-// TODO Finish up all the rest of the seams
-// TODO Get this to work with received file
-// TODO Make CustomNamer with... non-mutating
-// TODO Use Doxygen grouping to group related methods together for docs
-// TODO Move CustomNamer to .h and .cpp
 // TODO Move Path impl to .cpp
-// TODO Review usability
-// TODO Consider user scenarios
 // TODO Try it on an embedded device - or ask someone else to!
 // TODO Revisit our documentation
