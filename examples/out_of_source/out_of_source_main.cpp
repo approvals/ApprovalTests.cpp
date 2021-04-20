f65178db32ce5f37a0361192cc2f57f51e8eb743
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-04-20 11:33:52 +0100

    t Add commented-out code for experimentation

--- examples/out_of_source/out_of_source_main.cpp
+++ examples/out_of_source/out_of_source_main.cpp
@@ -1,4 +1,19 @@
 #define APPROVALS_DOCTEST
 #include "ApprovalTests.hpp"
 
+#include "DocTestReporter.h"
+
 using namespace ApprovalTests;
+
+//auto defaultReporterDisposer =
+//    Approvals::useAsDefaultReporter(std::make_shared<TextDiffReporter>());
+
+//auto defaultReporterDisposer =
+//    Approvals::useAsDefaultReporter(std::make_shared<DocTestReporter>());
+
+//auto default_namer_disposer = Approvals::useAsDefaultNamer(
+//    []()
+//    {
+//        return std::make_shared<TemplatedCustomNamer>(
+//            "{TestFileName}.{TestCaseName}.{ApprovedOrReceived}.{FileExtension}");
+//    });
