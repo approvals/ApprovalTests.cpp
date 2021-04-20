45139c9e53c54d3a097e8ba73c092510969bab4c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-04-20 11:34:35 +0100

    t Add debug output

--- examples/out_of_source/out_of_source_test.cpp
+++ examples/out_of_source/out_of_source_test.cpp
@@ -1,9 +1,13 @@
 #include "doctest/doctest.h"
 #include "ApprovalTests.hpp"
 
+#include <iostream>
+
 using namespace ApprovalTests;
 
 TEST_CASE("out_of_source sample")
 {
+    std::string approved = Approvals::getDefaultNamer()->getApprovedFile(".txt");
+    std::cout << "Approved filename = " << approved << std::endl;
     Approvals::verify(42);
 }
