116e92987c874dcf0b9e46e715b24860932295a1
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-04-21 18:24:50 +0100

    t Add script to easily execute tests in and out of source

---
+++ examples/out_of_source/build_install_test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Force execution to halt if there are any errors in this script:
+set -e
+set -o pipefail
+
+pushd ../../cmake-build-spaces/cmake-build-debug-clang-mp-90
+cmake --build . --target out_of_source
+
+pushd examples/out_of_source
+ctest .
+popd
+
+mkdir -p clare_install
+DESTDIR=clare_install make install
+
+pushd ./clare_install/usr/local/out_of_source
+
+# Test run that should pass
+echo 'Should pass: ----------------------------------------------------------------------------'
+APPROVED_FILES_ROOT_DIR=. APPROVAL_TESTS_USE_REPORTER=TextDiffReporter ./out_of_source
+
+echo 'Should fail: ----------------------------------------------------------------------------'
+# Test run that should fail
+echo "hello" >>out_of_source_test.out_of_source_sample.approved.txt
+APPROVED_FILES_ROOT_DIR=. APPROVAL_TESTS_USE_REPORTER=TextDiffReporter ./out_of_source
+
+popd
