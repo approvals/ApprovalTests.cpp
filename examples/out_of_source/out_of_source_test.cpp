d6202d61a529cebb74eb8c305130dc262abb6ecd
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-04-21 18:25:34 +0100

    t Remove some commands that are now in build_install_test.sh

--- examples/out_of_source/out_of_source_test.cpp
+++ examples/out_of_source/out_of_source_test.cpp
@@ -11,12 +11,3 @@ TEST_CASE("out_of_source sample")
     std::cout << "Approved filename = " << approved << std::endl;
     Approvals::verify(42);
 }
-
-// Commands used to test this:
-/*
-    cd /Users/clare/Documents/develop/ApprovalTests/ApprovalTests.cpp/cmake-build-spaces/cmake-build-debug-clang-mp-90
-    mkdir -p clare_install
-    DESTDIR=clare_install make install
-    cd ./clare_install/usr/local/out_of_source
-    APPROVAL_TESTS_USE_REPORTER=TextDiffReporter ./out_of_source
-*/
