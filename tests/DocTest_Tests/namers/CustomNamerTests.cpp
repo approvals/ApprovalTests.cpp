ecf470e1dbc7bbc2a13a2083dd2397388b5d09de
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-04-23 17:21:53 +0100

    d Update TODOs
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- tests/DocTest_Tests/namers/CustomNamerTests.cpp
+++ tests/DocTest_Tests/namers/CustomNamerTests.cpp
@@ -37,19 +37,15 @@ TEST_CASE("Test Namer Injection")
 // TODO Revisit our documentation
 // TODO Document ApprovalTestNamer::setCheckBuildConfig(false)
 // TODO Document the details and limitations of each template string
+
 // TODO Document scenarios where this might be useful (when run outside of build dir, e.g. embedded)
+// TODO Make documentation example use different filenames for approved and received, for safety
+
 // TODO Reporters.md should link to the new How To file
 // TODO New "How To" file should link to Reporters.md
-// TODO Make documentation example use different filenames for approved and received, for safety
 // TODO Add CreateCustomNamer page to doc/README.md
 // TODO Add CreateCustomNamer page to doc/sphinx/index.rst
 
 // ---- Tests
 // TODO Rename this file to TemplatedCustomNamerTests.cpp
 
-// ---- Features
-// TODO Make it easier to register TemplatedCustomNamer as custom namer
-// TODO Convert / or \ to native directory separators
-// TODO {TestSourceDirectory} is misleading - it does not always match the source directory - it should call ApprovalTestNamer::getTestSourceDirectory()
-// TODO Review the template strings, for any missing cases
-// TODO How to access ApprovalTestNamer::getRelativePathOfSourceDirectoryFromSourceRootForApproved() via {}
