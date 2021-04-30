89fdbf8b66280a00d5b39d03275bb4dc54cb15eb
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-04-30 22:05:47 +0100

    t Remove unused header in fmt_approvals_demo

--- examples/fmt_approvals_demo/main.cpp
+++ examples/fmt_approvals_demo/main.cpp
@@ -4,7 +4,5 @@
 #define APPROVALS_DOCTEST
 #include "ApprovalTests.hpp"
 
-#include "ApprovalTests/Approvals.h"
-
 auto directoryDisposer =
     ApprovalTests::Approvals::useApprovalsSubdirectory("approval_tests");
