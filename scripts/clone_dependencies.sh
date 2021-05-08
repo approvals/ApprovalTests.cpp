5b4c4bd11bcff69f8299a6bb31027b9aff1d4217
Clare Macrae <claremacrae@users.noreply.github.com>
2021-05-08 11:58:48 +0100

    Pin to specific fmt version
    The commit
    fmtlib/fmt@400b953a51955
    changed the default format for ranges in the fmt
    library... So for now, I'm pinning to the last release.

--- scripts/clone_dependencies.sh
+++ scripts/clone_dependencies.sh
@@ -9,6 +9,6 @@ git clone https://github.com/abseil/googletest.git
 git clone https://github.com/boost-ext/ut.git
 git clone https://github.com/catchorg/Catch2.git -b v2.x --single-branch
 git clone https://github.com/cpputest/cpputest.git
-git clone https://github.com/fmtlib/fmt.git
+git clone https://github.com/fmtlib/fmt.git --branch "7.1.3"
 git clone https://github.com/gulrak/filesystem.git
 git clone https://github.com/onqtam/doctest.git
