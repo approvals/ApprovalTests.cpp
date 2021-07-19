0c17a0388167b82fdc74bbb7e117251758b7aef2
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-07-19 12:19:57 -0600

    t start grid test
    Co-Authored-By: Clare Macrae <github@cfmacrae.fastmail.co.uk>

---
+++ tests/DocTest_Tests/utilities/GridTests.cpp
@@ -0,0 +1,14 @@
+#include "doctest/doctest.h"
+#include "ApprovalTests/Approvals.h"
+
+using namespace ApprovalTests;
+
+TEST_CASE("Print Grid")
+{
+    // todo: approve a grid of even = O odd = X
+}
+
+// empathy for managers
+// Fast agile
+// Small details of menders
+// Self selecting teams
