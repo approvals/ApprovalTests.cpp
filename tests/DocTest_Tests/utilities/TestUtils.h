560b75c184cf8ffc7cdfc5e9649af4d789da5289
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-08-26 10:15:53 -0600

    t proper headers
    Co-Authored-By: Clare Macrae <github@cfmacrae.fastmail.co.uk>

--- tests/DocTest_Tests/utilities/TestUtils.h
+++ tests/DocTest_Tests/utilities/TestUtils.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "doctest/doctest.h"
+#include "ApprovalTests/utilities/StringUtils.h"
 
 inline void require_ends_with(const std::string& text, const std::string& endsWith)
 {
