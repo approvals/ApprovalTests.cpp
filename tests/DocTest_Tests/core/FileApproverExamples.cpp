bee41514f5b920ec02421de2b0cb3bab198afd50
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-08-26 10:55:11 -0600

    t fully compiling examples
    Co-Authored-By: Clare Macrae <github@cfmacrae.fastmail.co.uk>

---
+++ tests/DocTest_Tests/core/FileApproverExamples.cpp
@@ -0,0 +1,32 @@
+#include "doctest/doctest.h"
+#include "../../DocTest_Tests/reporters/TestReporter.h"
+#include "ApprovalTests/core/ApprovalException.h"
+#include "ApprovalTests/core/FileApprover.h"
+
+// begin-snippet: create_custom_comparator
+class LengthComparator : public ApprovalTests::ApprovalComparator
+{
+public:
+    bool contentsAreEquivalent(std::string receivedPath,
+                               std::string approvedPath) const override
+    {
+        return ApprovalTests::FileUtils::fileSize(receivedPath) == ApprovalTests::FileUtils::fileSize(approvedPath);
+    }
+};
+// end-snippet
+
+TEST_CASE("ItUsesCustomComparator")
+{
+    using namespace ApprovalTests;
+
+    FileUtils::writeToFile("a.length", "12345");
+    FileUtils::writeToFile("b.length", "56789");
+
+    // begin-snippet: use_custom_comparator
+    auto disposer = FileApprover::registerComparatorForExtension(
+        ".length", std::make_shared<LengthComparator>());
+    // end-snippet
+
+    FileApprover::verify("a.length", "b.length");
+}
+
--- tests/DocTest_Tests/core/FileApproverTests.cpp
+++ tests/DocTest_Tests/core/FileApproverTests.cpp
@@ -57,30 +57,6 @@ TEST_CASE("ItComparesTheEntireFile")
     CHECK_THROWS_AS(FileApprover::verify("a.txt", "b.txt"), ApprovalMismatchException);
 }
 
-// begin-snippet: create_custom_comparator
-class LengthComparator : public ApprovalComparator
-{
-public:
-    bool contentsAreEquivalent(std::string receivedPath,
-                               std::string approvedPath) const override
-    {
-        return FileUtils::fileSize(receivedPath) == FileUtils::fileSize(approvedPath);
-    }
-};
-// end-snippet
-
-TEST_CASE("ItUsesCustomComparator")
-{
-    FileUtils::writeToFile("a.length", "12345");
-    FileUtils::writeToFile("b.length", "56789");
-
-    // begin-snippet: use_custom_comparator
-    auto disposer = FileApprover::registerComparatorForExtension(
-        ".length", std::make_shared<LengthComparator>());
-    // end-snippet
-
-    FileApprover::verify("a.length", "b.length");
-}
 
 namespace
 {
