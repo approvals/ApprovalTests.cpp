860ef5c0ce9320bf7733bd5a8ef64687192f97f4
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-08-26 18:28:39 +0100

    t Compiling snippets
    Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com>

--- tests/UT_Tests/UTApprovalTestTests.cpp
+++ tests/UT_Tests/UTApprovalTestTests.cpp
@@ -5,12 +5,11 @@
 
 #include "tests/DocTest_Tests/reporters/FakeReporter.h"
 
-using namespace ApprovalTests;
 int main()
 {
     using namespace boost::ut;
 
-    auto directory = Approvals::useApprovalsSubdirectory("approval_tests");
+    auto directory = ApprovalTests::Approvals::useApprovalsSubdirectory("approval_tests");
 
     "ItReportsAndThrowsIfVerifyFails"_test = []() {
         auto front_loader = std::make_shared<FakeReporter>(true);
@@ -18,14 +17,14 @@ int main()
         // Use a front-loaded reporter so this is used even we are running
         // in a CI system.
         auto front_loaded_reporter_disposer =
-            Approvals::useAsFrontLoadedReporter(front_loader);
+            ApprovalTests::Approvals::useAsFrontLoadedReporter(front_loader);
 
         bool approvalMissingExceptionReceived = false;
         try
         {
-            Approvals::verify("cucumber");
+            ApprovalTests::Approvals::verify("cucumber");
         }
-        catch (const ApprovalMissingException&)
+        catch (const ApprovalTests::ApprovalMissingException&)
         {
             approvalMissingExceptionReceived = true;
         }
@@ -47,12 +46,12 @@ int main()
 
     // begin-snippet: ut_main_usage
     "ItCanVerifyAFile"_test = []() {
-        Approvals::verify("Approval Tests can verify text via the golden master method");
+        ApprovalTests::Approvals::verify("Approval Tests can verify text via the golden master method");
     };
     // end-snippet
 
     test("AnotherWayItCanVerifyAFile") = []() {
-        Approvals::verify("Approval Tests can verify text via the golden master method");
+        ApprovalTests::Approvals::verify("Approval Tests can verify text via the golden master method");
     };
 
     // begin-snippet: ut_main_multiple
@@ -60,13 +59,13 @@ int main()
         {
             // Here we simulate test sections, so that Approval Tests uses different
             // output file names for the different verify() calls.
-            auto section = NamerFactory::appendToOutputFilename("section 1");
-            Approvals::verify(
+            auto section = ApprovalTests::NamerFactory::appendToOutputFilename("section 1");
+            ApprovalTests::Approvals::verify(
                 "Approval Tests can verify text via the golden master method");
         }
         {
-            auto section = NamerFactory::appendToOutputFilename("section 2");
-            Approvals::verify("Approval Tests can verify different text via "
+            auto section = ApprovalTests::NamerFactory::appendToOutputFilename("section 2");
+            ApprovalTests::Approvals::verify("Approval Tests can verify different text via "
                               "the golden master method");
         }
     };
@@ -74,32 +73,32 @@ int main()
 
     "YouCanUseAWriter"_test = []() {
         // begin-snippet: ut_use_custom_writer
-        using HtmlWriter = StringWriter;
+        using HtmlWriter = ApprovalTests::StringWriter;
         HtmlWriter writer("<h1>hello world</h1>", ".html");
-        Approvals::verify(writer);
+        ApprovalTests::Approvals::verify(writer);
         // end-snippet
     };
 
     "YouCanSpecifyYourFileExtension"_test = []() {
         // begin-snippet: ut_use_custom_file_extension
-        Approvals::verify("<h1>hello world</h1>",
-                          Options().fileOptions().withFileExtension(".html"));
+        ApprovalTests::Approvals::verify("<h1>hello world</h1>",
+                          ApprovalTests::Options().fileOptions().withFileExtension(".html"));
         // end-snippet
     };
 
     "YouCanSpecifyYourFileExtensionWithToString"_test = []() {
-        Approvals::verify(1337, Options().fileOptions().withFileExtension(".csv"));
+        ApprovalTests::Approvals::verify(1337, ApprovalTests::Options().fileOptions().withFileExtension(".csv"));
     };
 
     "YouCanSpecifyYourFileExtensionWithFormatter"_test = []() {
-        Approvals::verify(
+        ApprovalTests::Approvals::verify(
             1337,
             [](auto value, auto& os) { os << "**value:** " << value; },
-            Options().fileOptions().withFileExtension(".md"));
+            ApprovalTests::Options().fileOptions().withFileExtension(".md"));
     };
 
     "VerifyAFileWithAmpersand&"_test = [&]() {
-        auto namer = Approvals::getDefaultNamer();
+        auto namer = ApprovalTests::Approvals::getDefaultNamer();
         expect(throws([&] { auto name = namer->getReceivedFile(".txt"); }))
             << "documenting bug #157, if you see this, the bug has been fixed upstream "
                "and can be closed, and this test should be updated";
