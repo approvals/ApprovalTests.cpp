6eb659bf0594c5270fbf7a37e22a1e417fb00b05
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-08-29 11:50:52 +0100

    d doctest_existing_main/DoctestApprovalsTests.cpp now uses fully qualified names

--- examples/doctest_existing_main/DoctestApprovalsTests.cpp
+++ examples/doctest_existing_main/DoctestApprovalsTests.cpp
@@ -9,9 +9,7 @@
 #include "doctest/doctest.h"
 #include "ApprovalTests/Approvals.h"
 
-using namespace ApprovalTests;
-
 TEST_CASE("TestStreamableObject")
 {
-    Approvals::verify(42);
+    ApprovalTests::Approvals::verify(42);
 }
