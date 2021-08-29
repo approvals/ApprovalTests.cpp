6338d4d52339c348b6f5db12fb3156ec896d1f4a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-08-29 11:48:26 +0100

    d cpputest_starter docs now use fully qualified names

--- examples/cpputest_starter/cpputest_starter_test.cpp
+++ examples/cpputest_starter/cpputest_starter_test.cpp
@@ -1,8 +1,6 @@
 #include "ApprovalTests.hpp"
 #include "CppUTest/TestHarness.h"
 
-using namespace ApprovalTests;
-
 TEST_GROUP(CppUTestStarter){};
 
 TEST(CppUTestStarter, Sample)
@@ -10,5 +8,5 @@ TEST(CppUTestStarter, Sample)
     // TODO Replace 42 with the value or object whose contents you are verifying.
     // For help, see:
     // https://approvaltestscpp.readthedocs.io/en/latest/generated_docs/ToString.html
-    Approvals::verify(42);
+    ApprovalTests::Approvals::verify(42);
 }
