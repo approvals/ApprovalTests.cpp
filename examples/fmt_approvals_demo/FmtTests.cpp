28b05ce1b7ee057e56e81b837a670de8357ba863
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-08-29 11:49:30 +0100

    d FmtTests.cpp now uses fully qualified names

--- examples/fmt_approvals_demo/FmtTests.cpp
+++ examples/fmt_approvals_demo/FmtTests.cpp
@@ -7,12 +7,10 @@
 
 #include "ApprovalTests/integrations/fmt/FmtApprovals.h"
 
-using namespace ApprovalTests;
-
 TEST_CASE("FmtContainers")
 {
     // begin-snippet: fmt_approvals
     std::vector<int> numbers = {1, 2, 3};
-    FmtApprovals::verify(numbers);
+    ApprovalTests::FmtApprovals::verify(numbers);
     // end-snippet
 }
