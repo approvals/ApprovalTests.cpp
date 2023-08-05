f91bdf4a93eb7d4014a4e24e13da0f66564f92b0
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-08-05 16:56:06 +0100

    F Remove mingw getenv_s() declaration workaround
    It's no longer needed, and is giving:
    
    D:/a/ApprovalTests.cpp/ApprovalTests.cpp/ApprovalTests/utilities/WinMinGWUtils.h:15:13:
    error: 'errno_t getenv_s(size_t*, char*, size_t, const char*)' redeclared without dllimport attribute: previous dllimport ignored [-Werror=attributes]
       15 |     errno_t getenv_s(size_t* ret_required_buf_size,
          |             ^~~~~~~~

--- ApprovalTests/utilities/WinMinGWUtils.h
+++ ApprovalTests/utilities/WinMinGWUtils.h
@@ -3,22 +3,3 @@
 #if (defined(__MINGW32__) || defined(__MINGW64__))
 #define APPROVAL_TESTS_MINGW
 #endif
-
-#ifdef APPROVAL_TESTS_MINGW
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-#include <sec_api/stdlib_s.h> /* errno_t, size_t */
-
-    errno_t getenv_s(size_t* ret_required_buf_size,
-                     char* buf,
-                     size_t buf_size_in_bytes,
-                     const char* name);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif // APPROVAL_TESTS_MINGW
