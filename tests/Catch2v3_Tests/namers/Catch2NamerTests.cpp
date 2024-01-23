e9c4f74dc66c205a248cae3f3a567385235af1e2
Simon Weis <laguna_1989@gmx.net>
2024-01-23 19:16:53 +0100

    - r clean up includes for catch2v3 in Catch2NamerTest

--- tests/Catch2v3_Tests/namers/Catch2NamerTests.cpp
+++ tests/Catch2v3_Tests/namers/Catch2NamerTests.cpp
@@ -1,4 +1,5 @@
-#include "catch2/catch_all.hpp"
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_all.hpp>
 #include "ApprovalTests/namers/ApprovalTestNamer.h"
 #include "ApprovalTests/utilities/StringUtils.h"
 #include "ApprovalTests/utilities/SystemUtils.h"
