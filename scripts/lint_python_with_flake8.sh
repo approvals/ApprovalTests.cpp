0cbe1977bc50bbfcc10efe5b316c82cb8ce87301
Llewellyn Falco <llewellyn.falco@gmail.com>
2025-04-22 10:58:04 -0600

    . e exclude library from flake8
    Co-Authored-By: Clare Macrae <github@cfmacrae.fastmail.co.uk>
    Co-Authored-By: Laguna1989 <laguna.ws@gmail.com>

--- scripts/lint_python_with_flake8.sh
+++ scripts/lint_python_with_flake8.sh
@@ -1,7 +1,8 @@
-#!/bin/env sh -e
+#!/usr/bin/env sh
+set -e
 
 # stop the build if there are Python syntax errors or undefined names
-flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
+flake8 . --exclude=venv,build/venv,*/site-packages/* --count --select=E9,F63,F7,F82 --show-source --statistics
 # exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
-flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
+flake8 . --exclude=venv,build/venv,*/site-packages/* --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
 
